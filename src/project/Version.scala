768b74e0e05d28e58f70260cb7b223cd535fbeb5
lossyrob <rdemanuele@gmail.com>
2017-11-20 17:56:46 -0500

    Deployment script tweaks

--- src/project/Version.scala
+++ src/project/Version.scala
@@ -1,7 +1,7 @@
 object Version {
   val scala = "2.11.11"
   val osmesa = "0.1.0"
-  val geotrellis = "1.2.0-OSMESA"
+  val geotrellis = "1.2.0-SNAPSHOT"
   val geomesa = "1.4.0-SNAPSHOT"
   val vectorpipe = "1.0.0-SNAPSHOT"
   val decline = "0.3.0"
