6dd2d9fb324e0687879e6bc86fffe699c8a8c431
Nathan Zimmerman <npzimmerman@gmail.com>
2017-12-04 14:33:00 -0500

    Why no work TF?

--- deployment/terraform/stats/main.tf
+++ deployment/terraform/stats/main.tf
@@ -1,12 +1,42 @@
 
-# ECS cluster is only a name that ECS machines may join
-resource "aws_ecs_cluster" "osmesa_stats" {
+data "template_file" "container_instance_cloud_config" {
+  template = "${file("cloud-config/container-instance.yml.tpl")}"
 
-  lifecycle {
-    create_before_destroy = true
-  }
+  vars {}
+}
 
-  name = "OSMESA-STATS"
+module "container_service_cluster" {
+  source = "github.com/azavea/terraform-aws-ecs-cluster?ref=0.6.0"
+
+  vpc_id            = "vpc-617f9604"
+  ami_id            = "ami-b2df2ca4"
+  instance_type     = "t2.xlarge"
+  key_name          = "geotrellis-emr"
+  cloud_config      = "${data.template_file.container_instance_cloud_config.rendered}"
+
+  root_block_device_type = "gp2"
+  root_block_device_size = "10"
+
+  health_check_grace_period = "600"
+  desired_capacity          = "1"
+  min_size                  = "1"
+  max_size                  = "1"
+
+  enabled_metrics = [
+    "GroupMinSize",
+    "GroupMaxSize",
+    "GroupDesiredCapacity",
+    "GroupInServiceInstances",
+    "GroupPendingInstances",
+    "GroupStandbyInstances",
+    "GroupTerminatingInstances",
+    "GroupTotalInstances",
+  ]
+
+  private_subnet_ids = ["subnet-7d2ba926"]
+
+  project     = "OSMesa"
+  environment = "Staging"
 }
 
 # Template for container definition, allows us to inject environment
@@ -15,8 +45,8 @@ data "template_file" "osmesa_stats_task" {
 
   vars {
     image    = "quay.io/geotrellis/osmesa-query:latest",
-    s3bucket = "${var.s3bucket}",
-    s3prefix = "${var.s3prefix}"
+    s3bucket = "geotrellis-test",
+    s3prefix = "nathan/osm-stats"
   }
 }
 
@@ -29,52 +59,10 @@ resource "aws_ecs_task_definition" "osmesa_stats" {
 # Defines running an ECS task as a service
 resource "aws_ecs_service" "osmesa_stats" {
   name                               = "osmesa_stats"
-  cluster                            = "${aws_ecs_cluster.osmesa_stats.id}"
+  cluster                            = "${module.container_service_cluster.id}"
   task_definition                    = "${aws_ecs_task_definition.osmesa_stats.family}:${aws_ecs_task_definition.osmesa_stats.revision}"
   desired_count                      = "${var.desired_stats_instance_count}"
   deployment_minimum_healthy_percent = "0" # allow old services to be torn down
   deployment_maximum_percent         = "100"
 }
-
-#
-# AutoScaling resources
-#
-
-# Defines a launch configuration for ECS worker, associates it with our cluster
-resource "aws_launch_configuration" "ecs" {
-  name = "ECS ${aws_ecs_cluster.osmesa_stats.name}"
-  image_id             = "${var.aws_ecs_ami}"
-  instance_type        = "${var.ecs_instance_type}"
-  iam_instance_profile = "${var.ecs_instance_profile}"
-
-  # TODO: is there a good way to make the key configurable sanely?
-  key_name             = "${var.ec2_key}"
-  associate_public_ip_address = true
-}
-
-# Auto-scaling group for ECS workers
-resource "aws_autoscaling_group" "ecs" {
-  lifecycle {
-    create_before_destroy = true
-  }
-
-  # Explicitly linking ASG and launch configuration by name
-  # to force replacement on launch configuration changes.
-  name = "${aws_launch_configuration.ecs.name}"
-
-  launch_configuration      = "${aws_launch_configuration.ecs.name}"
-  health_check_grace_period = 600
-  health_check_type         = "EC2"
-  desired_capacity          = "${var.desired_stats_instance_count}"
-  min_size                  = "${var.desired_stats_instance_count}"
-  max_size                  = "${var.desired_stats_instance_count}"
-  vpc_zone_identifier       = ["${var.subnet_id}"]
-
-  tag {
-    key                 = "Name"
-    value               = "ECS ${aws_ecs_cluster.osmesa_stats.name}"
-    propagate_at_launch = true
-  }
-}
-
 
