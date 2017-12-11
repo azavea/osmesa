af60fc3419a44144d1eb7a438b868372c1fccae4
lossyrob <rdemanuele@gmail.com>
2017-12-11 13:39:17 -0500

    Added route for vector tile passthrough

--- src/project/Dependencies.scala
+++ src/project/Dependencies.scala
@@ -28,4 +28,5 @@ object Dependencies {
   val circeOptics    = "io.circe"                    %% "circe-optics"                 % Version.circe
   val circeJava8     = "io.circe"                    %% "circe-java8"                  % Version.circe
   val logging        = "com.typesafe.scala-logging"  %% "scala-logging"                % Version.scalaLogging
+  val commonsIO      = "commons-io"                  %  "commons-io"                   % Version.commonsIO
 }
