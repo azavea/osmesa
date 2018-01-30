ae5152784cd7d362b1936b7e6cab0a47459618fd
James McClain <jmcclain@azavea.com>
2018-01-30 13:46:57 -0500

    Similarity Between California And California
    https://raw.githubusercontent.com/johan/world.geo.json/master/countries/USA/CA.geo.json
    https://raw.githubusercontent.com/mlampros/DataSets/master/california.geojson
    
    ```bash
    ~/local/bin/sbt "project bm" "runMain osmesa.bm.VertexMatching $PWD/geojson/CA.geo.json $PWD/geojson/california.geojson"
    ...
    [info] [0.996627, 0.010724, -0.056167; -0.001380, 1.003387, 0.013399; -0.000131, 0.000331, 1.000000]
    [info] [0.938596, -0.427711, 20.486836; 0.027355, 0.718802, 4.695807; 0.008592, -0.009870, 1.000000]
    ...
    ```

---
+++ src/bm/src/main/scala/osmesa/bm/VertexMatching.scala
@@ -0,0 +1,60 @@
+package osmesa.bm
+
+import geotrellis.vector._
+import geotrellis.vector.io._
+import geotrellis.vector.io.json.JsonFeatureCollection
+
+import spray.json._
+import spray.json.DefaultJsonProtocol._
+
+
+object VertexMatching {
+
+  def pointToPolygon(p: Point, right: Polygon) = {
+
+    right.vertices
+      .sliding(2)
+      .map({ case Array(a, b) =>
+        val px: Double = p.x - a.x
+        val py: Double = p.y - a.y
+        val vx: Double = b.x - a.x
+        val vy: Double = b.y - a.y
+        val absv: Double = math.sqrt(vx*vx + vy*vy)
+        val t = px*vx/absv + py*vy/absv
+
+        val c =
+          if (t <= 0.0) a
+          else if (t >= 1.0) b
+          else Point(a.x*t + b.x*(1.0-t), a.y*t + b.y*(1.0-t))
+
+        (p.distance(c), c)
+      })
+      .reduce({ (t1: (Double, Point), t2: (Double, Point)) =>
+        if (t1._1 <= t2._1) t1
+        else t2
+      })._2
+  }
+
+  def polygonToPolygon(left: Polygon, right: Polygon) = {
+    val xs1 = left.vertices
+    val xs2 = xs1.map({ point => pointToPolygon(point, right) })
+    Homography.dlt(xs1, xs2)
+  }
+
+  def main(args: Array[String]): Unit = {
+    val polygon1 = scala.io.Source.fromFile(args(0)).mkString.parseGeoJson[Geometry] match {
+      case p: Polygon => p
+      case mp: MultiPolygon =>
+        mp.polygons.reduce({ (p1, p2) => if (p1.vertices.length > p2.vertices.length) p1; else p2 })
+    }
+    val polygon2 = scala.io.Source.fromFile(args(1)).mkString.parseGeoJson[Geometry] match {
+      case p: Polygon => p
+      case mp: MultiPolygon =>
+        mp.polygons.reduce({ (p1, p2) => if (p1.vertices.length > p2.vertices.length) p1; else p2 })
+    }
+
+    println(polygonToPolygon(polygon1, polygon2))
+    println(polygonToPolygon(polygon2, polygon1))
+ }
+
+}
