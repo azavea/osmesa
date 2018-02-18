19dbcf194d1917103120c47768a1c4492a31e3df
Seth Fitzsimmons <seth@mojodna.net>
2018-02-17 16:48:52 -0800

    Sample invocation for full planet history

--- src/ingest/src/main/scala/osmesa/ingest/RegionStatsApp.scala
+++ src/ingest/src/main/scala/osmesa/ingest/RegionStatsApp.scala
@@ -22,6 +22,17 @@ import osmesa.ProcessOSM
  *   --out-regions $HOME/data/geom-regions \
  *   --out-changesets $HOME/data/changeset-regions
  *
+ * spark-submit \
+ *   --driver-memory=10g \
+ *   --conf spark.sql.shuffle.partitions=5000 \
+ *   --conf spark.speculation=true \
+ *   --conf spark.yarn.executor.memoryOverhead=2g \
+ *   --class osmesa.ingest.RegionStatsApp \
+ *   s3://osm-pds-tmp/osmesa-ingest.jar \
+ *   --node-geoms s3://osm-pds-tmp/point-geoms-20180212 \
+ *   --way-geoms s3://osm-pds-tmp/way-geoms-20180212 \
+ *   --out-regions s3://osm-pds-tmp/geom-countries-20180212 \
+ *   --out-changesets s3://osm-pds-tmp/changeset-countries-20180212
  */
 object RegionStatsApp extends CommandApp(
   name = "osmesa-region-stats",
