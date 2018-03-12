db34dc659d23991140f297b2dd8152c304c7610d
James McClain <jmcclain@azavea.com>
2018-03-12 18:38:46 -0400

    Remediate Various Bugs

--- src/bm/src/main/scala/osmesa/bm/BuildingMatching.scala
+++ src/bm/src/main/scala/osmesa/bm/BuildingMatching.scala
@@ -20,6 +20,7 @@ import vectorpipe.osm._
 
 import cats.implicits._
 import com.monovore.decline._
+import monocle.macros.GenLens
 
 import osmesa.GenerateVT
 
@@ -69,6 +70,7 @@ object BuildingMatching extends CommandApp(
   header = "Match Buildings",
   main = {
     val ss = Util.sparkSession("Building-Matching")
+    val tags = GenLens[vectorpipe.osm.ElementMeta](_.tags)
 
     Logger.getLogger("org").setLevel(Level.ERROR)
     Logger.getLogger("akka").setLevel(Level.ERROR)
@@ -138,26 +140,26 @@ object BuildingMatching extends CommandApp(
 
         if (!nomatch) {
 
-          val features: RDD[GenerateVT.VTF[Geometry]] = dataset1Features.map({ f => (f, 0) })
+          val features/*: RDD[GenerateVT.VTF[Geometry]]*/ = dataset1Features.map({ f => (f, 0) })
             .union(dataset2Features.map({ f => (f, 1) }))
             .partitionBy(new QuadTreePartitioner(Range(0,24).toSet, 4099)) // 4099 is the smallest prime larger than 2**12 = 4096
             .mapPartitions({ (it: Iterator[(OSMFeature, Int)]) =>
+
+              // Data
               val array = it.toArray
 
               // Venn diagram of data
               val (left, intersection, right) = {
-                val middle = mutable.ArrayBuffer.empty[OSMFeature]
-                val middleFromLeft = mutable.Set.empty[Long]
-                val middleFromRight = mutable.Set.empty[Long]
+                val middle = mutable.Set.empty[OSMFeature]
+                val middle2 = mutable.Set.empty[OSMFeature]
 
                 var i = 0; while (i < array.length) {
                   val a = array(i)._1
                   var j = i+1; while (j < array.length) {
-                    val b = array(i)._1
+                    val b = array(j)._1
                     if (a.geom == b.geom) {
                       middle += a
-                      middleFromLeft += a.data.uid
-                      middleFromRight += b.data.uid
+                      middle2 += b
                     }
                     j = j + 1
                   }
@@ -166,11 +168,11 @@ object BuildingMatching extends CommandApp(
 
                 val leftMinusMiddle = array
                   .filter(_._2 == 0)
-                  .filter({ t => !middleFromLeft.contains(t._1.data.uid) })
+                  .filter({ case (f, _) => !(middle.contains(f) || middle2.contains(f)) })
                   .map(_._1)
                 val rightMinusMiddle = array
                   .filter(_._2 == 1)
-                  .filter({ t => !middleFromRight.contains(t._1.data.uid) })
+                  .filter({ case (f, _) => !(middle.contains(f) || middle2.contains(f)) })
                   .map(_._1)
 
                 (leftMinusMiddle, middle.toArray, rightMinusMiddle)
@@ -185,17 +187,18 @@ object BuildingMatching extends CommandApp(
                   var i = 0; while (i < left.length) {
                     val a = left(i)
                     val aCentroid = Centroid.getCentroid(a.jtsGeom)
-                    val dist1 = a.distance(c)
+                    val dist1 = aCentroid.distance(cCentroid)
                     val (vx, vy) = (aCentroid.x - cCentroid.x, aCentroid.y - cCentroid.y)
                     val absv = math.sqrt(vx*vx + vy*vy)
                     var j = 0; while (j < right.length) {
                       val b = right(j)
                       val bCentroid = Centroid.getCentroid(b.jtsGeom)
-                      val dist2 = b.distance(c)
+                      val dist2 = bCentroid.distance(cCentroid)
                       val (ux, uy) = (bCentroid.x - cCentroid.x, bCentroid.y - cCentroid.y)
                       val absu = math.sqrt(ux*ux + uy*uy)
-                      val dot = (vx*ux + vy*uy)/(absv*absu)
-                      data(i)(j)(k) = dot*math.min(dist1/dist2,dist2/dist1)
+                      val dot = ((vx*ux + vy*uy)/(absv*absu) + 1.0)/2.0
+                      val dist = math.min(dist1/dist2,dist2/dist1)
+                      data(i)(j)(k) = dot*dist
                       j = j + 1
                     }
                     i = i + 1
@@ -215,8 +218,8 @@ object BuildingMatching extends CommandApp(
                   }
                   i = i + 1
                 }
-                val totals = data.map(_.sum)
-                data.zip(totals).map({ case (a, total) => a.map({ x => x/total }) })
+                val maximum = if (left.nonEmpty && right.nonEmpty) data.map(_.max).max; else 1.0
+                data.map({ a => a.map({ x => x/maximum }) })
               }
 
               // Compute Probabilities
@@ -228,7 +231,8 @@ object BuildingMatching extends CommandApp(
                     val b = right(j).geom.asInstanceOf[Polygon]
                     val p1 = 1.0 - VertexMatching.score(a, b)
                     val (p2, p3) = VolumeMatching.data(a, b)
-                    data(i)(j) = (math.max(p1, math.max(p2, p3)) + q(i)(j))/2
+                    data(i)(j) = (math.max(p1, math.max(p2, p3)) + q(i)(j))/2.0
+                    // data(i)(j) = math.max(p1, math.max(p2, p3)) // XXX
                     j = j + 1
                   }
                   i = i + 1
@@ -236,30 +240,89 @@ object BuildingMatching extends CommandApp(
                 data
               }
 
-              Array.empty[(OSMFeature, (String, Double, OSMFeature))].toIterator // XXX
-            }) // XXX , preservesPartitioning = true)
+              // val retval = mutable.ArrayBuffer.empty[(OSMFeature, (Double, Long))]
+              val retval = mutable.ArrayBuffer.empty[(OSMFeature, (Double, Geometry))]
+
+              // intersection.foreach({ f =>
+              //   val geom = f.geom
+              //   val data = f.data
+              //   val f2 = new OSMFeature(geom, tags.set(data.tags + ("dataset" -> "both"))(data))
+              //   val pair = (f2, (-1L, 0.0))
+              //   retval.append(pair)
+              // })
+
+              // left
+              var i = 0; while (i < left.length) {
+                val f1 = left(i)
+                val geom = f1.geom
+                val data = f1.data
+                val f2 = new OSMFeature(geom, tags.set(data.tags + ("dataset" -> "left"))(data))
+                var j = 0; while (j < right.length) {
+                  val geom2 = right(j).geom
+                  if ((p(i)(j) > 0.50) && (geom.distance(geom2) < 0.01)) { // XXX
+                    val pair2 = (p(i)(j), geom2)
+                    val pair = (f2, pair2)
+                    retval.append(pair)
+                  }
+                  j = j + 1
+                }
+                i = i + 1
+              }
+
+              // right
+              var j = 0; while (j < right.length) {
+                val f1 = right(j)
+                val geom = f1.geom
+                val data = f1.data
+                val f2 = new OSMFeature(geom, tags.set(data.tags + ("dataset" -> "right"))(data))
+                var i = 0; while (i < left.length) {
+                  val geom2 = left(i).geom
+                  if ((p(i)(j) > 0.50) && (geom.distance(geom2) < 0.01)) { // XXX
+                    val pair2 = (p(i)(j), geom2)
+                    val pair = (f2, pair2)
+                    retval.append(pair)
+                  }
+                  i = i + 1
+                }
+                j = j + 1
+              }
+
+              retval.toIterator
+            }, preservesPartitioning = true)
             .groupByKey // XXX can be optimized away by changing inner loop
-            .map({ case (b, itr) =>
-              // val best = itr.toArray.sortBy({ _._1 }).head
-              Feature(
-                b.geom.reproject(LatLng, WebMercator),
-                Map(
-                  "__id" -> VInt64(b.data.uid),
-                  "matches" -> VInt64(itr.toArray.length)
-                )
-              )
+
+          features
+            .collect
+            .foreach({ case (building, buildingsItr) =>
+              val buildings = buildingsItr.toArray
+              if (buildings.nonEmpty) {
+                println(s"${building.geom.toGeoJson}")
+                buildings.toArray.sortBy({ case (prob, _) => -prob }).foreach({ case (prob, building2) =>
+                  println(s"\t $prob ${building2.toGeoJson}")
+                })
+              }
             })
+          // .map({ case (b, itr) =>
+          //   // val best = itr.toArray.sortBy({ _._1 }).head
+          //   Feature(
+          //     b.geom.reproject(LatLng, WebMercator),
+          //     Map(
+          //       "__id" -> VInt64(b.data.uid),
+          //       "matches" -> VInt64(itr.toArray.length)
+          //     )
+          //   )
+          // })
 
-          val layoutScheme = ZoomedLayoutScheme(WebMercator, 512)
-          val maxLayoutLevel = layoutScheme.levelForZoom(19)
-          val LayoutLevel(zoom, layout) = maxLayoutLevel
-          val keyed = GenerateVT.keyToLayout(features, layout)
+          // val layoutScheme = ZoomedLayoutScheme(WebMercator, 512)
+          // val maxLayoutLevel = layoutScheme.levelForZoom(19)
+          // val LayoutLevel(zoom, layout) = maxLayoutLevel
+          // val keyed = GenerateVT.keyToLayout(features, layout)
 
-          GenerateVT.saveHadoop(
-            GenerateVT.makeVectorTiles(keyed, layout, "name"),
-            zoom,
-            "/tmp/tiles"
-          )
+          // GenerateVT.saveHadoop(
+          //   GenerateVT.makeVectorTiles(keyed, layout, "name"),
+          //   zoom,
+          //   "/tmp/tiles"
+          // )
 
         }
       })
