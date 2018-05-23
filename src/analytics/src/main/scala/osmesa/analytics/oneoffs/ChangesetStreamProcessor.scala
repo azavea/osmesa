7324905ac579c57ed06d5a2068f011654f3edefa
Seth Fitzsimmons <seth@mojodna.net>
2018-05-23 08:56:45 -0700

    Expose start/end sequence as CLI args

--- src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangesetStreamProcessor.scala
+++ src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangesetStreamProcessor.scala
@@ -37,9 +37,22 @@ object ChangesetStreamProcessor
         val databaseUrlOpt = Opts
           .option[URI]("database-url", short = "d", metavar = "database URL", help = "Database URL")
           .orNone
+        val startSequenceOpt = Opts
+          .option[Int](
+            "start-sequence",
+            short = "s",
+            metavar = "sequence",
+            help = "Starting sequence. If absent, the current (remote) sequence will be used.")
+          .orNone
+        val endSequenceOpt = Opts
+          .option[Int]("end-sequence",
+                       short = "e",
+                       metavar = "sequence",
+                       help = "Ending sequence. If absent, this will be an infinite stream.")
+          .orNone
 
-        (changesetSourceOpt, databaseUrlOpt).mapN {
-          (changesetSource, databaseUri) =>
+        (changesetSourceOpt, databaseUrlOpt, startSequenceOpt, endSequenceOpt).mapN {
+          (changesetSource, databaseUri, startSequence, endSequence) =>
             /* Settings compatible for both local and EMR execution */
             val conf = new SparkConf()
               .setIfMissing("spark.master", "local[*]")
@@ -55,18 +68,25 @@ object ChangesetStreamProcessor
 
             import ss.implicits._
 
+            val options = Map("base_uri" -> changesetSource.toString) ++
+              startSequence
+                .map(s => Map("start_sequence" -> s.toString))
+                .getOrElse(Map.empty[String, String]) ++
+              endSequence
+                .map(s => Map("end_sequence" -> s.toString))
+                .getOrElse(Map.empty[String, String])
+
             val changesets =
               ss.readStream
                 .format("osmesa.analytics.streaming.ChangesetsProvider")
-                .option("base_uri", "https://planet.osm.org/replication/changesets/")
-//                .option("start_sequence", 2901438)
-//                .option("end_sequence", 2901440)
+                .options(options)
+                .option("base_uri", changesetSource.toString)
                 .load
 
             val changesetProcessor = changesets
 //              .withWatermark("created_at", "5 seconds")
-              .select('id, 'tags, 'created_at)
-            .writeStream
+              .select('sequence, 'id, 'tags, 'created_at)
+              .writeStream
               .format("console")
 //              .foreach(new ForeachWriter[Row] {
 //                override def open(partitionId: Long, version: Long): Boolean = true
