f02d4105a2c72dd5a127df7ddded58871057ee5c
Seth Fitzsimmons <seth@mojodna.net>
2018-06-01 16:14:04 -0700

    Use changes (not augmented diff) stream

--- src/analytics/src/main/scala/osmesa/analytics/oneoffs/HashtagFootprintUpdater.scala
+++ src/analytics/src/main/scala/osmesa/analytics/oneoffs/HashtagFootprintUpdater.scala
@@ -1,6 +1,7 @@
 package osmesa.analytics.oneoffs
 
 import java.io._
+import java.math.BigDecimal
 import java.net.URI
 import java.util.zip.{GZIPInputStream, GZIPOutputStream}
 
@@ -13,7 +14,6 @@ import geotrellis.spark.io.index.zcurve.ZSpatialKeyIndex
 import geotrellis.spark.tiling.ZoomedLayoutScheme
 import geotrellis.spark.{KeyBounds, SpatialKey}
 import geotrellis.vector.io._
-import geotrellis.vector.io.json.JsonFeatureCollectionMap
 import geotrellis.vector.{
   Feature,
   Geometry,
@@ -27,12 +27,12 @@ import geotrellis.vector.{
 }
 import geotrellis.vectortile.{StrictLayer, VInt64, Value, VectorTile}
 import org.apache.commons.io.IOUtils
-import org.apache.spark._
 import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.encoders.RowEncoder
 import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types._
+import osmesa.analytics.Analytics
 import osmesa.analytics.updater.Implicits._
 import osmesa.analytics.updater.{makeLayer, path, read, write}
 import osmesa.common.functions.osm._
@@ -49,8 +49,7 @@ import scala.concurrent.forkjoin.ForkJoinPool
  *
  * spark-submit \
  *   --class osmesa.analytics.oneoffs.HashtagFootprintUpdater \
- *   ingest/target/scala-2.11/osmesa-analytics.jar \
- *   --augmented-diff-source s3://somewhere/diffs/ \
+ *   ingest/target/scala-2.11/osmesa-analytics.jar
  */
 object HashtagFootprintUpdater
     extends CommandApp(
@@ -60,11 +59,27 @@ object HashtagFootprintUpdater
         type AugmentedDiffFeature = Feature[Geometry, AugmentedDiff]
         val rootURI = new File("").toURI
 
-        val augmentedDiffSourceOpt = Opts.option[URI]("augmented-diff-source",
-                                                      short = "a",
-                                                      metavar = "uri",
-                                                      help =
-                                                        "Location of augmented diffs to process")
+        val changeSourceOpt = Opts
+          .option[URI]("change-source",
+                       short = "d",
+                       metavar = "uri",
+                       help = "Location of minutely diffs to process")
+          .withDefault(new URI("https://planet.osm.org/replication/minute/"))
+        val changesStartSequenceOpt = Opts
+          .option[Int](
+            "changes-start-sequence",
+            short = "s",
+            metavar = "sequence",
+            help =
+              "Minutely diff starting sequence. If absent, the current (remote) sequence will be used.")
+          .orNone
+        val changesEndSequenceOpt = Opts
+          .option[Int](
+            "changes-end-sequence",
+            short = "e",
+            metavar = "sequence",
+            help = "Minutely diff ending sequence. If absent, this will be an infinite stream.")
+          .orNone
         val changesetSourceOpt =
           Opts
             .option[URI]("changeset-source",
@@ -72,18 +87,20 @@ object HashtagFootprintUpdater
                          metavar = "uri",
                          help = "Location of changesets to process")
             .withDefault(new URI("https://planet.osm.org/replication/changesets/"))
-        val startSequenceOpt = Opts
+        val changesetsStartSequenceOpt = Opts
           .option[Int](
-            "start-sequence",
-            short = "s",
+            "changeset-start-sequence",
+            short = "S",
             metavar = "sequence",
-            help = "Starting sequence. If absent, the current (remote) sequence will be used.")
+            help =
+              "Changeset starting sequence. If absent, the current (remote) sequence will be used.")
           .orNone
-        val endSequenceOpt = Opts
-          .option[Int]("end-sequence",
-                       short = "e",
+        val changesetsEndSequenceOpt = Opts
+          .option[Int]("changeset-end-sequence",
+                       short = "E",
                        metavar = "sequence",
-                       help = "Ending sequence. If absent, this will be an infinite stream.")
+                       help =
+                         "Changeset ending sequence. If absent, this will be an infinite stream.")
           .orNone
         val tileSourceOpt = Opts
           .option[URI](
@@ -94,161 +111,54 @@ object HashtagFootprintUpdater
           )
           .withDefault(rootURI)
 
-        (augmentedDiffSourceOpt,
+        (changeSourceOpt,
+         changesStartSequenceOpt,
+         changesEndSequenceOpt,
          changesetSourceOpt,
-         startSequenceOpt,
-         endSequenceOpt,
+         changesetsStartSequenceOpt,
+         changesetsEndSequenceOpt,
          tileSourceOpt).mapN {
-          (augmentedDiffSource, changesetSource, startSequence, endSequence, tileSource) =>
-            /* Settings compatible with both local and EMR execution */
-            val conf = new SparkConf()
-              .setIfMissing("spark.master", "local[*]")
-              .setAppName("merged-changeset-stream-processor")
-              .set("spark.serializer", classOf[org.apache.spark.serializer.KryoSerializer].getName)
-              .set("spark.kryo.registrator",
-                   classOf[geotrellis.spark.io.kryo.KryoRegistrator].getName)
+          (changeSource,
+           changesStartSequence,
+           changesEndSequence,
+           changesetSource,
+           changesetsStartSequence,
+           changesetsEndSequence,
+           tileSource) =>
+            implicit val spark: SparkSession = Analytics.sparkSession("HashtagFootprintUpdater")
+            import spark.implicits._
 
-            implicit val ss: SparkSession = SparkSession.builder
-              .config(conf)
-              .enableHiveSupport
-              .getOrCreate
-
-            import ss.implicits._
-
-            // read augmented diffs as text for better geometry support (by reading from GeoJSON w/ GeoTrellis)
-            val diffs =
-              ss.readStream.textFile(augmentedDiffSource.toString)
-
-            implicit val augmentedDiffFeatureEncoder
-              : Encoder[(Option[AugmentedDiffFeature], AugmentedDiffFeature)] =
-              Encoders.kryo[(Option[AugmentedDiffFeature], AugmentedDiffFeature)]
-
-            val AugmentedDiffSchema = StructType(
-              StructField("sequence", LongType) ::
-                StructField("_type", ByteType, nullable = false) ::
-                StructField("id", LongType, nullable = false) ::
-                StructField("prevGeom", BinaryType, nullable = true) ::
-                StructField("geom", BinaryType, nullable = true) ::
-                StructField("prevTags",
-                            MapType(StringType, StringType, valueContainsNull = false),
-                            nullable = true) ::
-                StructField("tags",
-                            MapType(StringType, StringType, valueContainsNull = false),
-                            nullable = false) ::
-                StructField("prevChangeset", LongType, nullable = true) ::
-                StructField("changeset", LongType, nullable = false) ::
-                StructField("prevUid", LongType, nullable = true) ::
-                StructField("uid", LongType, nullable = false) ::
-                StructField("prevUser", StringType, nullable = true) ::
-                StructField("user", StringType, nullable = false) ::
-                StructField("prevUpdated", TimestampType, nullable = true) ::
-                StructField("updated", TimestampType, nullable = false) ::
-                StructField("prevVisible", BooleanType, nullable = true) ::
-                StructField("visible", BooleanType, nullable = false) ::
-                StructField("prevVersion", IntegerType, nullable = true) ::
-                StructField("version", IntegerType, nullable = false) ::
-                StructField("prevMinorVersion", IntegerType, nullable = true) ::
-                StructField("minorVersion", IntegerType, nullable = false) ::
-                Nil)
-
-            val AugmentedDiffEncoder: Encoder[Row] = RowEncoder(AugmentedDiffSchema)
-
-            implicit val encoder: Encoder[Row] = AugmentedDiffEncoder
-
-            val geoms = diffs map { line =>
-              val features = line
-              // Spark doesn't like RS-delimited JSON; perhaps Spray doesn't either
-                .replace("\u001e", "")
-                .parseGeoJson[JsonFeatureCollectionMap]
-                .getAll[AugmentedDiffFeature]
-
-              (features.get("old"), features("new"))
-            } map {
-              case (Some(prev), curr) =>
-                val _type = curr.data.elementType match {
-                  case "node"     => ProcessOSM.NodeType
-                  case "way"      => ProcessOSM.WayType
-                  case "relation" => ProcessOSM.RelationType
-                }
-
-                val minorVersion = if (prev.data.version == curr.data.version) 1 else 0
-
-                // generate Rows directly for more control over DataFrame schema; toDF will infer these, but let's be
-                // explicit
-                new GenericRowWithSchema(
-                  Array(
-                    curr.data.sequence.orNull,
-                    _type,
-                    prev.data.id,
-                    prev.geom.toWKB(4326),
-                    curr.geom.toWKB(4326),
-                    prev.data.tags,
-                    curr.data.tags,
-                    prev.data.changeset,
-                    curr.data.changeset,
-                    prev.data.uid,
-                    curr.data.uid,
-                    prev.data.user,
-                    curr.data.user,
-                    prev.data.timestamp,
-                    curr.data.timestamp,
-                    prev.data.visible.getOrElse(true),
-                    curr.data.visible.getOrElse(true),
-                    prev.data.version,
-                    curr.data.version,
-                    -1, // previous minor version is unknown
-                    minorVersion
-                  ),
-                  AugmentedDiffSchema
-                ): Row
-              case (None, curr) =>
-                val _type = curr.data.elementType match {
-                  case "node"     => ProcessOSM.NodeType
-                  case "way"      => ProcessOSM.WayType
-                  case "relation" => ProcessOSM.RelationType
-                }
-
-                new GenericRowWithSchema(
-                  Array(
-                    curr.data.sequence.orNull,
-                    _type,
-                    curr.data.id,
-                    null,
-                    curr.geom.toWKB(4326),
-                    null,
-                    curr.data.tags,
-                    null,
-                    curr.data.changeset,
-                    null,
-                    curr.data.uid,
-                    null,
-                    curr.data.user,
-                    null,
-                    curr.data.timestamp,
-                    null,
-                    curr.data.visible.getOrElse(true),
-                    null,
-                    curr.data.version,
-                    null,
-                    0
-                  ),
-                  AugmentedDiffSchema
-                ): Row
-            }
-
-            val changesetOptions = Map("base_uri" -> changesetSource.toString) ++
-              startSequence
+            val changeOptions = Map("base_uri" -> changeSource.toString) ++
+              changesStartSequence
                 .map(s => Map("start_sequence" -> s.toString))
                 .getOrElse(Map.empty[String, String]) ++
-              endSequence
+              changesEndSequence
                 .map(s => Map("end_sequence" -> s.toString))
                 .getOrElse(Map.empty[String, String])
 
-            val changesets =
-              ss.readStream
-                .format("changesets")
-                .options(changesetOptions)
-                .load
+            val changes = spark.readStream
+              .format("changes")
+              .options(changeOptions)
+              .load
+
+            val changesetOptions = Map("base_uri" -> changesetSource.toString) ++
+              changesetsStartSequence
+                .map(s => Map("start_sequence" -> s.toString))
+                .getOrElse(Map.empty[String, String]) ++
+              changesetsEndSequence
+                .map(s => Map("end_sequence" -> s.toString))
+                .getOrElse(Map.empty[String, String])
+
+            val changesets = spark.readStream
+              .format("changesets")
+              .options(changesetOptions)
+              .load
+
+            val watermarkedChanges = changes
+            // geoms are standalone; no need to wait for anything
+              .withWatermark("timestamp", "0 seconds")
+              .where('_type === ProcessOSM.NodeType and 'lat.isNotNull and 'lon.isNotNull)
+              .select('timestamp, 'changeset, 'user, 'lat, 'lon)
 
             val watermarkedChangesets = changesets
             // changesets can remain open for 24 hours; buy some extra time
@@ -257,14 +167,6 @@ object HashtagFootprintUpdater
               .withWatermark("created_at", "25 hours")
               .withColumn("hashtag", explode(hashtags('tags)))
               .select('id as 'changeset, 'hashtag)
-
-            // TODO these don't need to be proper geoms from aug diffs
-            val watermarkedGeoms = geoms
-              .withColumn("timestamp", to_timestamp('sequence * 60 + 1347432900))
-              // geoms are standalone; no need to wait for anything
-              .withWatermark("timestamp", "0 seconds")
-              .where('_type === ProcessOSM.NodeType)
-              .select('timestamp, 'changeset, 'user, 'geom)
 
             val BASE_ZOOM = 15
             val Cols = 256
@@ -293,7 +195,7 @@ object HashtagFootprintUpdater
 
             implicit val tupleEncoder: Encoder[KeyedTile] = Encoders.kryo[KeyedTile]
 
-            val baseQuery = watermarkedGeoms
+            val baseQuery = watermarkedChanges
               .join(watermarkedChangesets, Seq("changeset"))
 
             val tiledHashtags = baseQuery
@@ -301,7 +203,9 @@ object HashtagFootprintUpdater
               .flatMap {
                 row =>
                   val key = row.getAs[String]("key")
-                  val geom = row.getAs[Array[Byte]]("geom").readWKB
+                  val lat = row.getAs[BigDecimal]("lat").doubleValue
+                  val lon = row.getAs[BigDecimal]("lon").doubleValue
+                  val geom = Point(lon, lat)
                   val LayoutScheme = ZoomedLayoutScheme(WebMercator)
                   val layout = LayoutScheme.levelForZoom(zoom).layout
 
@@ -616,7 +520,7 @@ object HashtagFootprintUpdater
 
             query.awaitTermination()
 
-            ss.stop()
+            spark.stop()
         }
       }
     )
