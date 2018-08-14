43e199ad4e7464e3ba72a10a0072ae9ceb1bab29
Nathan Zimmerman <npzimmerman@gmail.com>
2018-08-14 12:39:06 -0400

    Document SQL and stream processes

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ operate on updated with minutely replication files.
 __NOTE__ This repo is pre-alpha and under active development. Contact the authors if you are interested in helping out or using this project.
 
 
-### Global Analytics
+## Batch Analytics
 
 Spark batch jobs will be able to run on ephemeral clusters for analytics, either scheduled or ad-hoc.
 
@@ -21,7 +21,7 @@ The OSMesa tools will be able to transform an RDD of Features that is pulled out
 into Vector Tiles with an appropriate schema.
 
 
-## Getting Started
+### Getting Started
 
 Utilities are provided in the [emr directory](emr) which bring up an EMR
 cluster and enable you to push the OSMesa uberjar to said cluster. The
@@ -30,4 +30,35 @@ jars to be registered/loaded for a console-like experience similar to
 Jupyter or IPython notebooks but which will execute spark jobs across the
 entire spark cluster.
 
+## Streaming statistics
+
+In addition to batch analytics, Spark Streaming processes are provided
+to compute statistics and geometries as OSM is updated.
+
+
+1. [AugmentedDiffStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/AugmentedDiffStreamProcessor.scala)
+2. [ChangeSetStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeSetStreamProcessor.scala)
+3. [ChangeStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeStreamProcessor.scala)
+4. [MergedChangesetStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/MergedChangesetStreamProcessor.scala)
+
+## SQL Tables
+
+Statistics calculation, whether batch or streaming, updates a few tables
+that jointly can be used to discover user or hashtag stats. These are
+the schemas of the tables being updated.
+
+- [changesets](src/analytics/sql/changesets.sql)
+- [changesets_countries](src/analytics/sql/changesets_countries.sql)
+- [changesets_hashtags](src/analytics/sql/changesets_hashtags.sql)
+- [countries](src/analytics/sql/countries.sql)
+- [hashtags](src/analytics/sql/hashtags.sql)
+- [users](src/analytics/sql/users.sql)
+
+
+These tables are fairly normalized and thus not the most efficient for
+directly supporting statistics servers. To that end, it might be useful
+to create materialized views for any furhter aggregation. A couple such
+example views are provided:
+[hashtag_statistics](src/analytics/sql/materialized_views/hashtag_statistics.sql)
+and [user_statistics](src/analytics/sql/materialized_views/user_statistics.sql)
 
--- src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeStreamProcessor.scala
+++ src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeStreamProcessor.scala
@@ -18,71 +18,63 @@ import osmesa.common.ProcessOSM
  *   ingest/target/scala-2.11/osmesa-analytics.jar \
  *   --database-url $DATABASE_URL
  */
-object ChangeStreamProcessor
-    extends CommandApp(
-      name = "osmesa-diff-stream-processor",
-      header = "display diffs from a change stream",
-      main = {
-        val changesetSourceOpt =
-          Opts
-            .option[URI](
-              "change-source",
-              short = "c",
-              metavar = "uri",
-              help = "Location of changes to process"
-            )
-            .withDefault(new URI("https://planet.osm.org/replication/minute/"))
-        val startSequenceOpt = Opts
-          .option[Int](
-            "start-sequence",
-            short = "s",
-            metavar = "sequence",
-            help =
-              "Starting sequence. If absent, the current (remote) sequence will be used."
-          )
-          .orNone
-        val endSequenceOpt = Opts
-          .option[Int](
-            "end-sequence",
-            short = "e",
-            metavar = "sequence",
-            help =
-              "Ending sequence. If absent, this will be an infinite stream."
-          )
-          .orNone
+object ChangeStreamProcessor extends CommandApp(
+  name = "osmesa-diff-stream-processor",
+  header = "display diffs from a change stream",
+  main = {
+    val changesetSourceOpt =
+      Opts.option[URI](
+        "change-source",
+        short = "c",
+        metavar = "uri",
+        help = "Location of changes to process"
+      ).withDefault(new URI("https://planet.osm.org/replication/minute/"))
+    val startSequenceOpt =
+      Opts.option[Int](
+        "start-sequence",
+        short = "s",
+        metavar = "sequence",
+        help = "Starting sequence. If absent, the current (remote) sequence will be used."
+      ).orNone
+    val endSequenceOpt =
+      Opts.option[Int](
+        "end-sequence",
+        short = "e",
+        metavar = "sequence",
+        help = "Ending sequence. If absent, this will be an infinite stream."
+      ).orNone
 
-        (changesetSourceOpt, startSequenceOpt, endSequenceOpt).mapN {
-          (changesetSource, startSequence, endSequence) =>
-            implicit val ss: SparkSession =
-              Analytics.sparkSession("ChangeStreamProcessor")
+    (changesetSourceOpt, startSequenceOpt, endSequenceOpt).mapN { (changesetSource, startSequence, endSequence) =>
+      implicit val ss: SparkSession =
+        Analytics.sparkSession("ChangeStreamProcessor")
 
-            import ss.implicits._
+      import ss.implicits._
 
-            val options = Map("base_uri" -> changesetSource.toString) ++
-              startSequence
-                .map(s => Map("start_sequence" -> s.toString))
-                .getOrElse(Map.empty[String, String]) ++
-              endSequence
-                .map(s => Map("end_sequence" -> s.toString))
-                .getOrElse(Map.empty[String, String])
+      val options = Map("base_uri" -> changesetSource.toString) ++
+        startSequence
+          .map(s => Map("start_sequence" -> s.toString))
+          .getOrElse(Map.empty[String, String]) ++
+        endSequence
+          .map(s => Map("end_sequence" -> s.toString))
+          .getOrElse(Map.empty[String, String])
 
-            val changes =
-              ss.readStream
-                .format("changes")
-                .options(options)
-                .load
+      val changes =
+        ss.readStream
+          .format("changes")
+          .options(options)
+          .load
 
-            val changeProcessor = changes
-              .select('id, 'version, 'lat, 'lon, 'visible)
-              .where('_type === ProcessOSM.NodeType and !'visible)
-              .writeStream
-              .queryName("display change data")
-              .format("console")
-              .start
+      val changeProcessor = changes
+        .select('id, 'version, 'lat, 'lon, 'visible)
+        .where('_type === ProcessOSM.NodeType and !'visible)
+        .writeStream
+        .queryName("display change data")
+        .format("console")
+        .start
 
-            changeProcessor.awaitTermination()
+      changeProcessor.awaitTermination()
 
-            ss.stop()
-        }
-      }
-    )
+      ss.stop()
+    }
+  }
+)
