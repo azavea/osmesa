190682acbd0817dd266cd444a99ae53ab2d2959b
Nathan Zimmerman <npzimmerman@gmail.com>
2018-08-15 10:23:26 -0400

    Add more information about batch/streaming updates

--- README.md
+++ README.md
@@ -5,43 +5,39 @@
 This project is a collection of tools for working with OpenStreetMap (OSM). It is built to enable
 large scale batch analytic jobs to run on the latest OSM data, as well as streaming jobs which
 operate on updated with minutely replication files.
-__NOTE__ This repo is pre-alpha and under active development. Contact the authors if you are interested in helping out or using this project.
-
-
-## Batch Analytics
-
-Spark batch jobs will be able to run on ephemeral clusters for analytics, either scheduled or ad-hoc.
-
-GeoMesa SparkSQL will also be available for analytics.
-
-
-### VectorTile Generation
-
-The OSMesa tools will be able to transform an RDD of Features that is pulled out of OSM ORC files
-into Vector Tiles with an appropriate schema.
 
 
 ### Getting Started
 
-Utilities are provided in the [emr directory](emr) which bring up an EMR
-cluster and enable you to push the OSMesa uberjar to said cluster. The
+Utilities are provided in the [deployment directory](deployment) to bring
+up cluster and enable you to push the OSMesa jar to that cluster. The
 spawned EMR cluster comes with Apache Zeppelin enabled, which allows
 jars to be registered/loaded for a console-like experience similar to
 Jupyter or IPython notebooks but which will execute spark jobs across the
-entire spark cluster.
-
-## Streaming statistics
-
-In addition to batch analytics, Spark Streaming processes are provided
-to compute statistics and geometries as OSM is updated.
+entire spark cluster. Actually wiring up Zeppelin to use OSMesa sources
+is beyond the scope of this document, but it is a relatively simple
+configuration.
 
 
-1. [AugmentedDiffStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/AugmentedDiffStreamProcessor.scala)
-2. [ChangeSetStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeSetStreamProcessor.scala)
-3. [ChangeStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeStreamProcessor.scala)
-4. [MergedChangesetStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/MergedChangesetStreamProcessor.scala)
+## Statistics
 
-## SQL Tables
+Summary statistics aggregated at the user and hashtag level that are
+supported by OSMesa:
+
+- Number of added buildings
+- Number of modified buildings
+- Number of added roads
+- Number of modified roads
+- Km of added roads
+- Km of modified roads
+- Number of added waterways
+- Number of modified waterways
+- Km of added waterways
+- Km of modified waterways
+- Number of added points of interest
+- Number of modified points of interest
+
+#### SQL Tables
 
 Statistics calculation, whether batch or streaming, updates a few tables
 that jointly can be used to discover user or hashtag stats. These are
@@ -56,9 +52,53 @@ the schemas of the tables being updated.
 
 
 These tables are fairly normalized and thus not the most efficient for
-directly supporting statistics servers. To that end, it might be useful
-to create materialized views for any furhter aggregation. A couple such
-example views are provided:
+directly serving statistics. If that's your goal, it might be useful
+to create materialized views for any further aggregation. A couple example
+queries that can serve as views are provided:
 [hashtag_statistics](src/analytics/sql/materialized_views/hashtag_statistics.sql)
 and [user_statistics](src/analytics/sql/materialized_views/user_statistics.sql)
+
+#### Batch
+
+- [ChangesetStats](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangesetStats.scala)
+will produce an ORCfile with statistics aggregated by changeset
+
+#### Stream
+
+- [AugmentedDiffStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/AugmentedDiffStreamProcessor.scala)
+updates the tables `changesets`, `users`, and `changesets_countries`
+- [ChangeSetStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeSetStreamProcessor.scala)
+updates the tables `changesets`, `changesets_hashtags`, `users`, and `hashtags`
+- [ChangeStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/ChangeStreamProcessor.scala)
+prints out changes to the console (primarily for debugging)
+- [MergedChangesetStreamProcessor](src/analytics/src/main/scala/osmesa/analytics/oneoffs/MergedChangesetStreamProcessor.scala)
+prints out changesets to the console (primarily for debugging)
+
+
+## Vector Tiles
+
+Vector tiles, too, are generated in batch and via streaming so that a
+fresh set can be quickly generated and then kept up to date. Summary
+vector tiles are produced for two cases: to illustrate the scope
+of a user's contribution and to illustrate the scope of a
+hashtag/campaign within OSM
+
+#### Batch
+
+- [FootprintByCampaign](src/analytics/src/main/scala/osmesa/analytics/oneoffs/FootprintByCampaign.scala)
+produces a z/x/y stack of vector tiles corresponding to all changes
+marked with a given hashtag
+- [FootprintByUser](src/analytics/src/main/scala/osmesa/analytics/oneoffs/FootprintByUser.scala)
+produces a z/x/y stack of vector tiles which correspond to a user's
+modifications to OSM
+
+#### Stream
+
+- [HashtagFootprintUpdater](src/analytics/src/main/scala/osmesa/analytics/oneoffs/HashtagFootprintUpdater.scala)
+updates a z/x/y stack of vector tiles corresponding to all changes
+marked with a given hashtag
+- [UserFootprintUpdater](src/analytics/src/main/scala/osmesa/analytics/oneoffs/UserFootprintUpdater.scala)
+updates a z/x/y stack of vector tiles which correspond to a user's
+modifications to OSM
+
 
--- src/analytics/src/main/scala/osmesa/analytics/updater/TileUpdater.scala
+++ src/analytics/src/main/scala/osmesa/analytics/updater/TileUpdater.scala
@@ -11,139 +11,130 @@ import osmesa.analytics.updater.schemas._
 
 class TileUpdater
 
-object TileUpdater
-    extends CommandApp(
-      name = "update-tiles",
-      header = "Update vector tiles with changes from an augmented diff",
-      main = {
-        val rootURI = new File("").toURI
+object TileUpdater extends CommandApp(
+  name = "update-tiles",
+  header = "Update vector tiles with changes from an augmented diff",
+  main = {
+    val rootURI = new File("").toURI
 
-        val replicationSourceOpt = Opts
-          .option[URI](
-            "replication-source",
-            short = "r",
-            metavar = "uri",
-            help = "URI prefix for replication files"
-          )
-          .withDefault(rootURI)
-        val tileSourceOpt = Opts
-          .option[URI](
-            "tile-source",
-            short = "t",
-            metavar = "uri",
-            help = "URI prefix for vector tiles to update"
-          )
-          .withDefault(rootURI)
-        val minZoomOpt = Opts.option[Int](
-          "min-zoom",
-          short = "z",
-          metavar = "zoom",
-          help = "Minimum zoom to consider"
-        )
-        val maxZoomOpt = Opts.option[Int](
-          "max-zoom",
-          short = "Z",
-          metavar = "zoom",
-          help = "Maximum zoom to consider"
-        )
-        val schemaOpt = Opts
-          .option[String](
-            "schema",
-            short = "s",
-            metavar = "schema",
-            help = "Schema"
-          )
-          .withDefault("snapshot")
-          .validate("Must be a registered schema") {
-            Schemas.keySet.contains(_)
-          }
-          .map {
-            Schemas(_)
-          }
-        val listingOpt = Opts
-          .option[Path](
-            "tiles",
-            short = "T",
-            metavar = "tile list",
-            help = "List of tiles available for updating"
-          )
-          .orNone
-        val dryRunOpt = Opts
-          .flag(
-            "dry-run",
-            short = "n",
-            help = "Dry run"
-          )
-          .orFalse
-        val verboseOpt = Opts
-          .flag(
-            "verbose",
-            short = "v",
-            help = "Be verbose"
-          )
-          .orFalse
-        val sequenceOpt = Opts.argument[Int]("sequence")
+    val replicationSourceOpt =
+      Opts.option[URI](
+        "replication-source",
+        short = "r",
+        metavar = "uri",
+        help = "URI prefix for replication files"
+      ).withDefault(rootURI)
+    val tileSourceOpt = Opts
+      .option[URI](
+        "tile-source",
+        short = "t",
+        metavar = "uri",
+        help = "URI prefix for vector tiles to update"
+      ).withDefault(rootURI)
+    val minZoomOpt =
+      Opts.option[Int](
+        "min-zoom",
+        short = "z",
+        metavar = "zoom",
+        help = "Minimum zoom to consider"
+      )
+    val maxZoomOpt =
+      Opts.option[Int](
+        "max-zoom",
+        short = "Z",
+        metavar = "zoom",
+        help = "Maximum zoom to consider"
+      )
+    val schemaOpt =
+      Opts.option[String](
+        "schema",
+        short = "s",
+        metavar = "schema",
+        help = "Schema"
+      ).withDefault("snapshot")
+      .validate("Must be a registered schema") { Schemas.keySet.contains(_) }
+      .map { Schemas(_) }
+    val listingOpt =
+      Opts.option[Path](
+        "tiles",
+        short = "T",
+        metavar = "tile list",
+        help = "List of tiles available for updating"
+      ).orNone
+    val dryRunOpt =
+      Opts.flag(
+        "dry-run",
+        short = "n",
+        help = "Dry run"
+      ).orFalse
+    val verboseOpt =
+      Opts.flag(
+        "verbose",
+        short = "v",
+        help = "Be verbose"
+      ).orFalse
+    val sequenceOpt = Opts.argument[Int]("sequence")
 
-        val logger = Logger.getLogger(classOf[TileUpdater])
+    val logger = Logger.getLogger(classOf[TileUpdater])
 
-        (replicationSourceOpt,
-         tileSourceOpt,
-         minZoomOpt,
-         maxZoomOpt,
-         schemaOpt,
-         listingOpt,
-         dryRunOpt,
-         verboseOpt,
-         sequenceOpt).mapN {
-          (replicationSource,
-           tileSource,
-           minZoom,
-           maxZoom,
-           schema,
-           listing,
-           dryRun,
-           verbose,
-           sequence) =>
-            val replicationUri = replicationSource.resolve(s"$sequence.json")
+    (replicationSourceOpt,
+     tileSourceOpt,
+     minZoomOpt,
+     maxZoomOpt,
+     schemaOpt,
+     listingOpt,
+     dryRunOpt,
+     verboseOpt,
+     sequenceOpt).mapN {
+      (replicationSource,
+       tileSource,
+       minZoom,
+       maxZoom,
+       schema,
+       listing,
+       dryRun,
+       verbose,
+       sequence) =>
+        val replicationUri = replicationSource.resolve(s"$sequence.json")
 
-            if (verbose) {
-              println(s"Applying $replicationUri to $tileSource from zoom $minZoom to $maxZoom...")
-            }
-
-            readFeatures(replicationUri) match {
-              case Some(features) =>
-                for (zoom <- minZoom to maxZoom) {
-                  updateTiles(
-                    tileSource = tileSource,
-                    zoom = zoom,
-                    schemaType = schema,
-                    features = features,
-                    listing = listing,
-                    process = (sk, tile) => {
-                      val filename = s"$zoom/${sk.col}/${sk.row}.mvt"
-                      val uri = tileSource.resolve(filename)
-
-                      if (dryRun) {
-                        println(
-                          s"Would write ${tile.toBytes.length.formatted("%,d")} bytes to $uri")
-                      } else {
-                        logger.info(
-                          s"Writing ${tile.toBytes.length.formatted("%,d")} bytes to $uri")
-                        // TODO gzip compress
-                        write(uri, tile.toBytes)
-                      }
-
-                      if (verbose) {
-                        println(filename)
-                      }
-                    }
-                  )
-                }
-
-              case None =>
-                println(s"No features available for $sequence")
-                System.exit(1)
-            }
+        if (verbose) {
+          println(s"Applying $replicationUri to $tileSource from zoom $minZoom to $maxZoom...")
         }
-      }
-    )
+
+        readFeatures(replicationUri) match {
+          case Some(features) =>
+            for (zoom <- minZoom to maxZoom) {
+              updateTiles(
+                tileSource = tileSource,
+                zoom = zoom,
+                schemaType = schema,
+                features = features,
+                listing = listing,
+                process = (sk, tile) => {
+                  val filename = s"$zoom/${sk.col}/${sk.row}.mvt"
+                  val uri = tileSource.resolve(filename)
+
+                  if (dryRun) {
+                    println(
+                      s"Would write ${tile.toBytes.length.formatted("%,d")} bytes to $uri")
+                  } else {
+                    logger.info(
+                      s"Writing ${tile.toBytes.length.formatted("%,d")} bytes to $uri")
+                    // TODO gzip compress
+                    write(uri, tile.toBytes)
+                  }
+
+                  if (verbose) {
+                    println(filename)
+                  }
+                }
+              )
+            }
+
+          case None =>
+            println(s"No features available for $sequence")
+            System.exit(1)
+        }
+    }
+  }
+)
