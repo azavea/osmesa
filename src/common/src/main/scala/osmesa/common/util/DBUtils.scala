ffb8b03af5e80ed52a8c1265216b37f8bde01ee7
Eugene Cheipesh <echeipesh@gmail.com>
2018-09-18 18:16:18 -0400

    Clean UserInfo and query from JDBC URI

--- src/common/src/main/scala/osmesa/common/util/DBUtils.scala
+++ src/common/src/main/scala/osmesa/common/util/DBUtils.scala
@@ -5,12 +5,21 @@ import java.sql.{Connection, DriverManager}
 
 object DBUtils {
   def getJdbcConnection(uri: URI): Connection = {
+    
+    val cleanUri = new URI(
+      scheme = uri.getScheme, 
+      authority = uri.getAuthority, // host and port
+      path = uri.getPath, 
+      query = null, 
+      fragment = null)
+    // also drops UserInfo
+
     val auth = Auth.fromUri(uri)
     (auth.user, auth.password) match {
       case (Some(user), Some(pass)) =>
-        DriverManager.getConnection(s"jdbc:${uri.toString}", user, pass)
+        DriverManager.getConnection(s"jdbc:${cleanUri.toString}", user, pass)
       case _ =>
-        DriverManager.getConnection(s"jdbc:${uri.toString}")
+        DriverManager.getConnection(s"jdbc:${cleanUri.toString}")
     }
   }
 }
