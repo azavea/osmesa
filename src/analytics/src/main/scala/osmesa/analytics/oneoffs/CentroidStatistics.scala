72d3de23472c244e09f1977d50e3205ae0c15af3
jpolchlo <jpolchlopek@azavea.com>
2018-10-22 18:39:48 -0400

    Initial version of building evolution demo

--- src/analytics/src/main/scala/osmesa/analytics/oneoffs/CentroidStatistics.scala
+++ src/analytics/src/main/scala/osmesa/analytics/oneoffs/CentroidStatistics.scala
@@ -4,15 +4,22 @@ import java.net.URI
 
 import cats.implicits._
 import com.monovore.decline.{CommandApp, Opts}
+import com.vividsolutions.jts.geom.{Geometry, Point}
+import geotrellis.proj4.{LatLng, WebMercator}
+import geotrellis.spark._
+import geotrellis.spark.io.index.ZCurveKeyIndexMethod
+import geotrellis.spark.tiling.ZoomedLayoutScheme
+import geotrellis.vector
+import geotrellis.vectortile._
 import org.apache.spark.sql.expressions.Window
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.{SaveMode, SparkSession}
+import org.locationtech.geomesa.spark.jts._
+import osmesa.GenerateVT
 import osmesa.analytics.Analytics
 import osmesa.common.ProcessOSM
 import osmesa.common.functions._
 import osmesa.common.functions.osm._
-import org.locationtech.geomesa.spark.jts._
-
 
 object CentroidStats extends CommandApp(
   name = "centroid-stats",
@@ -20,140 +27,151 @@ object CentroidStats extends CommandApp(
   main = {
     val historyOpt =
       Opts.option[String]("history", help = "Location of the History ORC file to process.")
+    val zoomOpt =
+      Opts.option[Int](long = "zoom", help = "Zoom level of most resolute tile layer")
     val outputOpt =
       Opts.option[URI](long = "output", help = "Output URI prefix; trailing / must be included")
 
-    (historyOpt, outputOpt).mapN { (historySource, output) =>
+    (historyOpt, outputOpt, zoomOpt).mapN { (historySource, output, baseZoom) =>
       implicit val spark: SparkSession = Analytics.sparkSession("ChangesetStats").withJTS
       import spark.implicits._
 
+      spark.sparkContext.setLogLevel("WARN")
+
+      if (output.getScheme != "s3") {
+        throw new IllegalArgumentException("Output URI must point to S3 bucket")
+      }
+
+      val logGridCells = 4 // Vector tiles will have 16x16 cells inside
+      val cellZoom = baseZoom + logGridCells
+
       val history = spark.read.orc(historySource)
 
-      val pointGeoms = ProcessOSM.geocode(ProcessOSM.constructPointGeometries(
-        // pre-filter to POI nodes
-        history.where('type === "node" and isPOI('tags))
-      ).withColumn("minorVersion", lit(0)))
+      // val pointGeoms = ProcessOSM.geocode(ProcessOSM.constructPointGeometries(
+      //   // pre-filter to POI nodes
+      //   history.where('type === "node" and isPOI('tags))
+      // ).withColumn("minorVersion", lit(0)))
 
-      val wayGeoms = ProcessOSM.geocode(ProcessOSM.reconstructWayGeometries(
+      val buildingGeoms = ProcessOSM.geocode(ProcessOSM.reconstructWayGeometries(
         // pre-filter to interesting ways
-        history.where('type === "way" and (isBuilding('tags) or isRoad('tags) or isWaterway('tags) or isPOI('tags))),
+        history.where('type === "way" and (isBuilding('tags))),// or isRoad('tags) or isWaterway('tags) or isPOI('tags))),
         // let reconstructWayGeometries do its thing; nodes are cheap
         history.where('type === "node")
       ).drop('geometryChanged))
 
       @transient val idByUpdated = Window.partitionBy('id).orderBy('updated)
 
-      val augmentedWays = wayGeoms
-        .withColumn("length", st_length('geom))
-        .withColumn("delta",
-          when(isRoad('tags) or isWaterway('tags),
-            coalesce(abs('length - (lag('length, 1) over idByUpdated)), lit(0)))
-            .otherwise(lit(0)))
+      val zls = ZoomedLayoutScheme(WebMercator)
+      val layout = zls.levelForZoom(cellZoom).layout
+      val keyIndex = {
+        val maxIdx = math.pow(2, cellZoom).toInt
+        val bounds= Bounds(SpatialKey(0,0), SpatialKey(maxIdx - 1, maxIdx - 1)).asInstanceOf[KeyBounds[SpatialKey]]
+        ZCurveKeyIndexMethod.createIndex(bounds)
+      }
+      val toWM = org.apache.spark.sql.functions.udf[Geometry, Geometry]{g =>
+        vector.Geometry(g).reproject(LatLng, WebMercator).jtsGeom
+      }
+      val indexWMPoint = org.apache.spark.sql.functions.udf[Long, Geometry]{ g =>
+        val wmpt = vector.Point(g.asInstanceOf[Point])
+        keyIndex.toIndex(layout.mapTransform(wmpt)).toLong
+      }
+      val wmPointToKey = org.apache.spark.sql.functions.udf[SpatialKey, Geometry]{ g =>
+        val wmpt = vector.Point(g.asInstanceOf[Point])
+        layout.mapTransform(wmpt)
+      }
+      val shiftRight = org.apache.spark.sql.functions.udf[Long, Long, Int]{(x, s) => x >> s}
+      val reduceSpatialKey = org.apache.spark.sql.functions.udf[SpatialKey, SpatialKey, Int]{ (k, levels) =>
+        SpatialKey(k.col >> levels, k.row >> levels)
+      }
 
-      val wayChangesetStats = augmentedWays
-        .withColumn("road_m_added",
-          when(isRoad('tags) and 'version === 1 and 'minorVersion === 0, 'length)
-            .otherwise(lit(0)))
-        .withColumn("road_m_modified",
-          when(isRoad('tags) and not('version === 1 and 'minorVersion === 0), 'delta)
-            .otherwise(lit(0)))
-        .withColumn("waterway_m_added",
-          when(isWaterway('tags) and 'version === 1 and 'minorVersion === 0, 'length)
-            .otherwise(lit(0)))
-        .withColumn("waterway_m_modified",
-          when(isWaterway('tags) and not('version === 1 and 'minorVersion === 0), 'delta)
-            .otherwise(lit(0)))
-        .withColumn("roads_added",
-          when(isRoad('tags) and 'version === 1 and 'minorVersion === 0, lit(1))
-            .otherwise(lit(0)))
-        .withColumn("roads_modified",
-          when(isRoad('tags) and not('version === 1 and 'minorVersion === 0), lit(1))
-            .otherwise(lit(0)))
-        .withColumn("waterways_added",
-          when(isWaterway('tags) and 'version === 1 and 'minorVersion === 0, lit(1))
-            .otherwise(lit(0)))
-        .withColumn("waterways_modified",
-          when(isWaterway('tags) and not('version === 1 and 'minorVersion === 0), lit(1))
-            .otherwise(lit(0)))
-        .withColumn("buildings_added",
-          when(isBuilding('tags) and 'version === 1 and 'minorVersion === 0, lit(1))
-            .otherwise(lit(0)))
-        .withColumn("buildings_modified",
-          when(isBuilding('tags) and not('version === 1 and 'minorVersion === 0), lit(1))
-            .otherwise(lit(0)))
-        .withColumn("pois_added",
-          when(isPOI('tags) and 'version === 1 and 'minorVersion === 0, lit(1))
-            .otherwise(lit(0)))
-        .withColumn("pois_modified",
-          when(isPOI('tags) and not('version === 1 and 'minorVersion === 0), lit(1))
-            .otherwise(lit(0)))
-        .groupBy('changeset)
+      val augmentedBuildings = buildingGeoms
+        .where(not(isnull('geom)))
+        .withColumn("area", st_area('geom))
+        .withColumn("center", st_centroid(toWM('geom)))
+        .withColumn("zindex", indexWMPoint('center))
+        .withColumn("spatialKey", wmPointToKey('center))
+        .withColumn("temporalKey", (col("updated").cast("long"))/lit(604800))
+
+      augmentedBuildings.printSchema
+
+      val cells = augmentedBuildings
+        .groupBy('spatialKey, 'temporalKey)
         .agg(
-          sum('road_m_added / 1000).as('road_km_added),
-          sum('road_m_modified / 1000).as('road_km_modified),
-          sum('waterway_m_added / 1000).as('waterway_km_added),
-          sum('waterway_m_modified / 1000).as('waterway_km_modified),
-          sum('roads_added).as('roads_added),
-          sum('roads_modified).as('roads_modified),
-          sum('waterways_added).as('waterways_added),
-          sum('waterways_modified).as('waterways_modified),
-          sum('buildings_added).as('buildings_added),
-          sum('buildings_modified).as('buildings_modified),
-          sum('pois_added).as('pois_added),
-          sum('pois_modified).as('pois_modified),
-          count_values(flatten(collect_list('countries))) as 'countries
+          first('zindex).as('zindex),
+          avg(st_x('center)).as('x),
+          avg(st_y('center)).as('y),
+          count('center).as('num)
+         )
+        .withColumn("center", st_makePoint('x, 'y))
+        .drop("x", "y")
+
+      cells.printSchema
+
+      // val aggregateStats = augmentedBuildings
+      //   .withColumn("buildings_added",
+      //     when(isBuilding('tags) and 'version === 1 and 'minorVersion === 0, lit(1))
+      //       .otherwise(lit(0)))
+      //   .withColumn("buildings_modified",
+      //     when(isBuilding('tags) and not('version === 1 and 'minorVersion === 0), lit(1))
+      //       .otherwise(lit(0)))
+      //   .groupBy('changeset)
+      //   .agg(
+      //     sum('buildings_added).as('buildings_added),
+      //     sum('buildings_modified).as('buildings_modified),
+      //     count_values(flatten(collect_list('countries))) as 'countries
+      //   )
+
+      val tileData = cells
+        .groupBy(shiftRight(col("zindex"), lit(2 * logGridCells)).as('tileIndex))
+        .agg(
+          first(shiftRight($"spatialKey.col", lit(logGridCells))).as('col),
+          first(shiftRight($"spatialKey.row", lit(logGridCells))).as('row),
+          collect_list('center).as('centers),
+          collect_list('num).as('buildingCounts),
+          collect_list((col("temporalKey") * lit(604800)).cast("timestamp")).as('weekBeginning)
         )
 
-      val pointChangesetStats = pointGeoms
-        .withColumn("pois_added",
-          when(isPOI('tags) and 'version === 1, lit(1))
-            .otherwise(lit(0)))
-        .withColumn("pois_modified",
-          when(isPOI('tags) and 'version > 1, lit(1))
-            .otherwise(lit(0)))
-        .groupBy('changeset)
-        .agg(
-          sum('pois_added) as 'pois_added,
-          sum('pois_modified) as 'pois_modified,
-          count_values(flatten(collect_list('countries))) as 'countries
-        )
+      tileData.printSchema
+      tileData.show
 
-      // coalesce values to deal with nulls introduced by the outer join
-      val rawChangesetStats = wayChangesetStats
-        .withColumnRenamed("pois_added", "way_pois_added")
-        .withColumnRenamed("pois_modified", "way_pois_modified")
-        .withColumnRenamed("countries", "way_countries")
-        .join(pointChangesetStats
-          .withColumnRenamed("pois_added", "node_pois_added")
-          .withColumnRenamed("pois_modified", "node_pois_modified")
-          .withColumnRenamed("countries", "node_countries"),
-          Seq("changeset"),
-          "full_outer")
-        .withColumn("road_km_added", coalesce('road_km_added, lit(0)))
-        .withColumn("road_km_modified", coalesce('road_km_modified, lit(0)))
-        .withColumn("waterway_km_added", coalesce('waterway_km_added, lit(0)))
-        .withColumn("waterway_km_modified", coalesce('waterway_km_modified, lit(0)))
-        .withColumn("roads_added", coalesce('roads_added, lit(0)))
-        .withColumn("roads_modified", coalesce('roads_modified, lit(0)))
-        .withColumn("waterways_added", coalesce('waterways_added, lit(0)))
-        .withColumn("waterways_modified", coalesce('waterways_modified, lit(0)))
-        .withColumn("buildings_added", coalesce('buildings_added, lit(0)))
-        .withColumn("buildings_modified", coalesce('buildings_modified, lit(0)))
-        .withColumn("pois_added",
-          coalesce('way_pois_added, lit(0)) + coalesce('node_pois_added, lit(0)))
-        .withColumn("pois_modified",
-          coalesce('way_pois_modified, lit(0)) + coalesce('node_pois_modified, lit(0)))
-        .withColumn("countries", merge_counts('node_countries, 'way_countries))
+      val tileLayout = zls.levelForZoom(baseZoom).layout
+      val keyedTiles = tileData
+        .rdd
+        .map{ row => {
+          val locations = row.getAs[Seq[Point]]("centers")
+          val buildingCounts = row.getAs[Seq[Long]]("buildingCounts")
+          val date = row.getAs[Seq[java.sql.Timestamp]]("weekBeginning")
+          val key = SpatialKey(row.getAs[Long]("col").toInt, row.getAs[Long]("row").toInt)
+          val ex = tileLayout.mapTransform(key)
 
+          val features = for (i <- Range(0, locations.length)) yield
+            vector.PointFeature(
+              vector.Point(locations(i)),
+              Map("buildingsChanged" -> VInt64(buildingCounts(i)),
+                  "weekBeginning" -> VInt64(date(i).getTime)))
 
-      rawChangesetStats
-        .repartition(50)
-        .write
-        .mode(SaveMode.Overwrite)
-        .orc(output.resolve("centroid-stats").toString)
+          (key, VectorTile(Map("statistics" -> StrictLayer(name="statistics",
+                                                           tileWidth=4096,
+                                                           version=2,
+                                                           tileExtent = ex,
+                                                           points = features,
+                                                           multiPoints = Seq(),
+                                                           lines = Seq(),
+                                                           multiLines = Seq(),
+                                                           polygons = Seq(),
+                                                           multiPolygons = Seq()
+                                                          )),
+                           ex)
+          )
+        }}
+
+      val prefix = if (output.getPath.endsWith("/"))
+                     output.getPath.drop(1).take(output.getPath.length - 2)
+                   else
+                     output.getPath.drop(1)
+      GenerateVT.save(keyedTiles, baseZoom, output.getAuthority, prefix)
 
       spark.stop()
     }
   }
 )
-
