5539499732cc8ffb97688711dc84b8e3c9a5d95c
jpolchlo <jpolchlopek@azavea.com>
2018-12-14 14:47:31 -0500

    Update batch deployment makefile configs
    Signed-off-by: jpolchlo <jpolchlopek@azavea.com>

--- deployment/batch/makefiles/config-emr.mk
+++ deployment/batch/makefiles/config-emr.mk
@@ -1,9 +1,14 @@
 export NAME := OSM Vector Tile Ingest - ${USER}
-export MASTER_INSTANCE:=m3.xlarge
+export MASTER_SECURITY_GROUP := sg-05ce84aaf4b8ddea9
+export WORKER_SECURITY_GROUP := sg-02edec736e909d506
+export SERVICE_ACCESS_SG := sg-055b18600d3fc2c50
+export SANDBOX_SG := sg-6b227c23
+
+export MASTER_INSTANCE := m3.xlarge
 export MASTER_PRICE := 0.10
-export WORKER_INSTANCE:=m3.xlarge
+export WORKER_INSTANCE := m3.xlarge
 export WORKER_PRICE := 0.07
-export WORKER_COUNT := 50
+export WORKER_COUNT := 32
 export USE_SPOT := true
 
 export DRIVER_MEMORY := 10000M
