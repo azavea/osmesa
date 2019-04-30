9285c62885b02378da5bc82a0fc0fc9b2f124d98
Seth Fitzsimmons <seth@mojodna.net>
2019-04-29 19:33:25 -0700

    Use asDouble function to handle nulls properly

--- src/analytics/src/main/scala/osmesa/analytics/oneoffs/EditHistogramTileCreator.scala
+++ src/analytics/src/main/scala/osmesa/analytics/oneoffs/EditHistogramTileCreator.scala
@@ -6,9 +6,9 @@ import cats.implicits._
 import com.monovore.decline._
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.sql.functions._
-import org.apache.spark.sql.types.DoubleType
 import org.locationtech.geomesa.spark.jts._
 import osmesa.analytics.{Analytics, EditHistogram}
+import osmesa.common.functions.asDouble
 
 object EditHistogramTileCreator
     extends CommandApp(
@@ -23,24 +23,26 @@ object EditHistogramTileCreator
         (
           historyOpt,
           outputOpt
-        ).mapN { (historyURI, outputURI) =>
-          implicit val spark: SparkSession = Analytics.sparkSession("State of the Data tile generation")
-          import spark.implicits._
-          spark.withJTS
+        ).mapN {
+          (historyURI, outputURI) =>
+            implicit val spark: SparkSession =
+              Analytics.sparkSession("State of the Data tile generation")
+            import spark.implicits._
+            spark.withJTS
 
-          val history = spark.read.orc(historyURI.toString)
+            val history = spark.read.orc(historyURI.toString)
 
-          val nodes = history
-            .where('type === "node" and 'lat.isNotNull and 'lon.isNotNull)
-            .withColumn("lat", 'lat.cast(DoubleType))
-            .withColumn("lon", 'lon.cast(DoubleType))
-            .where('uid > 1)
-            .select('lat, 'lon, year('timestamp) * 1000 + dayofyear('timestamp) as 'key)
+            val nodes = history
+              .where('type === "node" and 'lat.isNotNull and 'lon.isNotNull)
+              .withColumn("lat", asDouble('lat))
+              .withColumn("lon", asDouble('lon))
+              .where('uid > 1)
+              .select('lat, 'lon, year('timestamp) * 1000 + dayofyear('timestamp) as 'key)
 
-          val stats = EditHistogram.create(nodes, outputURI)
-          println(s"${stats.count} tiles created.")
+            val stats = EditHistogram.create(nodes, outputURI)
+            println(s"${stats.count} tiles created.")
 
-          spark.stop()
+            spark.stop()
         }
       }
     )
