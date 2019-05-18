1e04ae37173e333e71d30e37e5a44192cb1da000
Seth Fitzsimmons <seth@mojodna.net>
2019-05-17 18:40:17 -0700

    Nudge geometries read from MVTs
    This re-centers them within the "pixel" in the underlying tile and
    prevents features from being dropped due to failing `extent.contains`
    when on the edge of a tile.

--- src/analytics/src/main/scala/osmesa/analytics/EditHistogram.scala
+++ src/analytics/src/main/scala/osmesa/analytics/EditHistogram.scala
@@ -5,7 +5,7 @@ import java.net.URI
 import geotrellis.raster.RasterExtent
 import geotrellis.spark.io.index.zcurve.ZSpatialKeyIndex
 import geotrellis.spark.{KeyBounds, SpatialKey}
-import geotrellis.vector.{Extent, Feature, Point, PointFeature, Geometry => GTGeometry}
+import geotrellis.vector._
 import geotrellis.vectortile.{VInt64, Value, VectorTile}
 import org.apache.spark.sql._
 import osmesa.analytics.updater.Implicits._
@@ -66,6 +66,155 @@ object EditHistogram extends VectorGrid {
         }
       }
       .toDF("count", "z", "x", "y")
+  }
+
+  def updateTiles(tileSource: URI,
+                  mvts: GenMap[URI, VectorTile],
+                  tiles: TraversableOnce[VectorTileWithSequences])(
+      implicit taskSupport: TaskSupport): GenIterable[(Int, Int, Int, Int)] = {
+    // parallelize tiles to facilitate greater upload parallelism
+    val parTiles = tiles.toIterable.par
+    parTiles.tasksupport = taskSupport
+
+    parTiles.map {
+      // update tiles
+      case VectorTileWithSequences(z, sk, feats, sequences) =>
+        val extent = sk.extent(LayoutScheme.levelForZoom(z).layout)
+        val uri = makeURI(tileSource, z, sk)
+        val key = "edits"
+
+        mvts.get(uri) match {
+          case Some(tile) =>
+            // update existing tiles
+
+            // load the target layer
+            val layers = tile.layers.get(key) match {
+              case Some(layer) =>
+                // TODO feature construction / updating is very similar to osmesa.analytics.updater.schemas.*; see if the 2
+                // can be merged
+
+                // nudge geometries to cover the center of a point rather than the top-left (moving them into the
+                // tile's extent)
+                val layerFeatures = layer.features
+                    .map(f =>
+                      f.mapGeom(
+                        _.as[Point].get.translate(layer.resolution / 2, layer.resolution / -2)))
+
+                val newFeaturesById: Map[Long, Feature[Geometry, Map[String, Long]]] =
+                  feats
+                    .groupBy(_.data("__id"))
+                    .mapValues(_.head)
+                val featureIds: Set[Long] = newFeaturesById.keySet
+
+                val existingFeatures: Set[Long] =
+                  layer.features.map(f => f.data("__id"): Long).toSet
+
+                val unmodifiedFeatures =
+                  layerFeatures
+                    .filterNot(f => featureIds.contains(f.data("__id")))
+
+                val modifiedFeatures =
+                  layerFeatures.filter(f => featureIds.contains(f.data("__id")))
+
+                val replacementFeatures: Seq[Feature[Geometry, Map[String, Value]]] =
+                  modifiedFeatures.map(f =>
+                    f.mapData(d =>
+                      aggregateValues(d.mapValues(_.toLong) ++ newFeaturesById(d("__id")).data)))
+
+                val newFeatures =
+                  makeFeatures(
+                    feats
+                      .filterNot(f => existingFeatures.contains(f.data("__id")))).toSeq
+
+                unmodifiedFeatures ++ replacementFeatures ++ newFeatures match {
+                  case updatedFeatures if (replacementFeatures.length + newFeatures.length) > 0 =>
+                    val updatedLayer = makeLayer(key, extent, updatedFeatures, Cells)
+                    val sequenceLayer =
+                      makeSequenceLayer(getCommittedSequences(tile) ++ sequences, extent, Cells)
+
+                    Some(updatedLayer, sequenceLayer)
+                  case _ =>
+                    logError(s"No changes to $uri; THIS SHOULD NOT HAVE HAPPENED.")
+                    None
+                }
+              case None =>
+                Some(makeLayer(key, extent, makeFeatures(feats), Cells),
+                     makeSequenceLayer(sequences, extent, Cells))
+            }
+
+            layers match {
+              case Some((dataLayer, sequenceLayer)) =>
+                // merge all available layers into a new tile
+                val newTile =
+                  VectorTile(
+                    tile.layers
+                      .updated(dataLayer._1, dataLayer._2)
+                      // update a second layer with a feature corresponding to committed sequences
+                      .updated(sequenceLayer._1, sequenceLayer._2),
+                    extent
+                  )
+
+                write(newTile, uri)
+
+              case None => // no new data
+            }
+
+          case None =>
+            write(VectorTile(Map(makeLayer(key, extent, makeFeatures(feats), Cells),
+                                 makeSequenceLayer(sequences, extent, Cells)),
+                             extent),
+                  uri)
+        }
+
+        (feats.size, z, sk.col, sk.row)
+    }
+  }
+
+  def makeFeatures(features: Iterable[PointFeature[Map[String, Long]]])
+    : Iterable[Feature[Point, Map[String, VInt64]]] =
+    features
+      .map(f => f.mapData(aggregateValues))
+      .toSeq
+      // put recently-edited features first
+      .sortBy(_.data.keys.filter(k => !k.startsWith("__")).max)
+      .reverse
+
+  def aggregateValues(data: Map[String, Long]): Map[String, VInt64] = {
+    val dates = data.filterKeys(k => !k.startsWith("__") && !k.contains(":"))
+    val facets =
+      data.keys.filter(k => !k.startsWith("__") && k.contains(":")).map(_.split(":").last)
+
+    // sum each facet
+    val facetTotals = facets
+      .flatMap(
+        facet =>
+          Seq(
+            s"__total:${facet}" -> VInt64(
+              data.filterKeys(k => !k.startsWith("__") && k.endsWith(s":${facet}")).values.sum),
+            s"__lastEdit:${facet}" -> VInt64(
+              data
+                .filterKeys(k => !k.startsWith("__") && k.endsWith(s":${facet}"))
+                .keys
+                .max
+                .split(":")
+                .head
+                .toLong)
+        ))
+      .toMap
+
+    // convert values and summarize
+    data.mapValues(VInt64) ++
+      Map(
+        // sum all edit counts
+        "__total" -> VInt64(dates.values.sum),
+        "__lastEdit" -> VInt64(dates.keys.max.toLong)
+      ) ++
+      facetTotals
+  }
+
+  def makeURI(tileSource: URI, zoom: Int, sk: SpatialKey): URI = {
+    val filename = s"${path(zoom, sk)}"
+    tileSource.resolve(filename)
   }
 
   def update(nodes: DataFrame, tileSource: URI, baseZoom: Int = DefaultBaseZoom)(
@@ -129,152 +278,11 @@ object EditHistogram extends VectorGrid {
       .toDF("count", "z", "x", "y")
   }
 
-  def updateTiles(tileSource: URI,
-                  mvts: GenMap[URI, VectorTile],
-                  tiles: TraversableOnce[VectorTileWithSequences])(
-      implicit taskSupport: TaskSupport): GenIterable[(Int, Int, Int, Int)] = {
-    // parallelize tiles to facilitate greater upload parallelism
-    val parTiles = tiles.toIterable.par
-    parTiles.tasksupport = taskSupport
-
-    parTiles.map {
-      // update tiles
-      case VectorTileWithSequences(z, sk, feats, sequences) =>
-        val extent = sk.extent(LayoutScheme.levelForZoom(z).layout)
-        val uri = makeURI(tileSource, z, sk)
-        val key = "edits"
-
-        mvts.get(uri) match {
-          case Some(tile) =>
-            // update existing tiles
-
-            // load the target layer
-            val layers = tile.layers.get(key) match {
-              case Some(layer) =>
-                // TODO feature construction / updating is very similar to osmesa.analytics.updater.schemas.*; see if the 2
-                // can be merged
-
-                val newFeaturesById: Map[Long, Feature[GTGeometry, Map[String, Long]]] =
-                  feats
-                    .groupBy(_.data("__id"))
-                    .mapValues(_.head)
-                val featureIds: Set[Long] = newFeaturesById.keySet
-
-                val existingFeatures: Set[Long] =
-                  layer.features.map(f => f.data("__id"): Long).toSet
-
-                val unmodifiedFeatures =
-                  layer.features.filterNot(f => featureIds.contains(f.data("__id")))
-
-                val modifiedFeatures =
-                  layer.features.filter(f => featureIds.contains(f.data("__id")))
-
-                val replacementFeatures: Seq[Feature[GTGeometry, Map[String, Value]]] =
-                  modifiedFeatures.map(f =>
-                    f.mapData(d =>
-                      aggregateValues(d.mapValues(_.toLong) ++ newFeaturesById(d("__id")).data)))
-
-                val newFeatures =
-                  makeFeatures(
-                    feats
-                      .filterNot(f => existingFeatures.contains(f.data("__id")))).toSeq
-
-                unmodifiedFeatures ++ replacementFeatures ++ newFeatures match {
-                  case updatedFeatures if (replacementFeatures.length + newFeatures.length) > 0 =>
-                    val updatedLayer = makeLayer(key, extent, updatedFeatures, Cells)
-                    val sequenceLayer =
-                      makeSequenceLayer(getCommittedSequences(tile) ++ sequences, extent)
-
-                    Some(updatedLayer, sequenceLayer)
-                  case _ =>
-                    logError(s"No changes to $uri; THIS SHOULD NOT HAVE HAPPENED.")
-                    None
-                }
-              case None =>
-                Some(makeLayer(key, extent, makeFeatures(feats), Cells),
-                     makeSequenceLayer(sequences, extent))
-            }
-
-            layers match {
-              case Some((dataLayer, sequenceLayer)) =>
-                // merge all available layers into a new tile
-                val newTile =
-                  VectorTile(
-                    tile.layers
-                      .updated(dataLayer._1, dataLayer._2)
-                      // update a second layer with a feature corresponding to committed sequences
-                      .updated(sequenceLayer._1, sequenceLayer._2),
-                    extent
-                  )
-
-                write(newTile, uri)
-
-              case None => // no new data
-            }
-
-          case None =>
-            write(VectorTile(Map(makeLayer(key, extent, makeFeatures(feats), Cells),
-                                 makeSequenceLayer(sequences, extent)),
-                             extent),
-                  uri)
-        }
-
-        (feats.size, z, sk.col, sk.row)
-    }
-  }
-
-  def makeFeatures(features: Iterable[PointFeature[Map[String, Long]]])
-    : Iterable[Feature[Point, Map[String, VInt64]]] =
-    features
-      .map(f => f.mapData(aggregateValues))
-      .toSeq
-      // put recently-edited features first
-      .sortBy(_.data.keys.filter(k => !k.startsWith("__")).max)
-      .reverse
-
-  def aggregateValues(data: Map[String, Long]): Map[String, VInt64] = {
-    val dates = data.filterKeys(k => !k.startsWith("__") && !k.contains(":"))
-    val facets =
-      data.keys.filter(k => !k.startsWith("__") && k.contains(":")).map(_.split(":").last)
-
-    // sum each facet
-    val facetTotals = facets
-      .flatMap(
-        facet =>
-          Seq(
-            s"__total:${facet}" -> VInt64(
-              data.filterKeys(k => !k.startsWith("__") && k.endsWith(s":${facet}")).values.sum),
-            s"__lastEdit:${facet}" -> VInt64(
-              data
-                .filterKeys(k => !k.startsWith("__") && k.endsWith(s":${facet}"))
-                .keys
-                .max
-                .split(":")
-                .head
-                .toLong)
-        ))
-      .toMap
-
-    // convert values and summarize
-    data.mapValues(VInt64) ++
-      Map(
-        // sum all edit counts
-        "__total" -> VInt64(dates.values.sum),
-        "__lastEdit" -> VInt64(dates.keys.max.toLong)
-      ) ++
-      facetTotals
-  }
-
   def makeUrls(tileSource: URI, tiles: Seq[VectorTileWithSequence]): Map[URI, Extent] =
     tiles.map { tile =>
       (makeURI(tileSource, tile.zoom, tile.sk),
        tile.sk.extent(LayoutScheme.levelForZoom(tile.zoom).layout))
     } toMap
-
-  def makeURI(tileSource: URI, zoom: Int, sk: SpatialKey): URI = {
-    val filename = s"${path(zoom, sk)}"
-    tileSource.resolve(filename)
-  }
 
   def getUncommittedTiles(tileSource: URI,
                           tiles: Seq[VectorTileWithSequence],
