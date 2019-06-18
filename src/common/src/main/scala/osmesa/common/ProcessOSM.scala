12801521288bb7bb02cb980cd272d088d9e1f3f6
Seth Fitzsimmons <seth@mojodna.net>
2019-06-18 08:47:41 -0700

    Rename st_pointToGeom to match st_castToGeometry
    st_castToGeometry was added to GeoMesa in 2.2.0.

--- src/common/src/main/scala/osmesa/common/ProcessOSM.scala
+++ src/common/src/main/scala/osmesa/common/ProcessOSM.scala
@@ -16,7 +16,7 @@ import org.apache.spark.sql.jts.GeometryUDT
 import org.apache.spark.sql.types._
 import org.locationtech.geomesa.spark.jts._
 import com.vividsolutions.jts.{geom => jts}
-import osmesa.common.functions.asDouble
+import osmesa.common.functions._
 import osmesa.common.functions.osm._
 import osmesa.common.relations.MultiPolygons
 import osmesa.common.relations.Routes
@@ -222,13 +222,11 @@ object ProcessOSM {
     import elements.sparkSession.implicits._
     elements.sparkSession.withJTS
 
-    val st_pointToGeom = org.apache.spark.sql.functions.udf { pt: jts.Point => pt.asInstanceOf[jts.Geometry] }
-
     val nodes = ProcessOSM.preprocessNodes(elements)
 
     val nodeGeoms = ProcessOSM.constructPointGeometries(nodes)
       .withColumn("minorVersion", lit(0))
-      .withColumn("geom", st_pointToGeom('geom))
+      .withColumn("geom", st_castToGeometry('geom))
 
     val wayGeoms = ProcessOSM.reconstructWayGeometries(elements, nodes)
 
