ba36e83ffa904617c33444caa584d7f0762eb58c
Seth Fitzsimmons <seth@mojodna.net>
2019-06-18 08:51:56 -0700

    Add box2d function for generating an unencoded Box2D struct
    Useful for utilizing predicate pushdown to pre-filter geometries by
    bounding box before applying spatial operators that require WKB to be
    deserialized.

--- src/common/src/main/scala/osmesa/common/functions/package.scala
+++ src/common/src/main/scala/osmesa/common/functions/package.scala
@@ -5,6 +5,8 @@ import org.apache.spark.sql.Column
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.types.{DoubleType, FloatType}
+import org.locationtech.geomesa.spark.jts.{st_envelope, st_exteriorRing, st_geometryType, st_isEmpty, st_pointN,
+st_x, st_y}
 import osmesa.common.util._
 
 package object functions {
@@ -59,4 +61,19 @@ package object functions {
 
   // this was added to GeoMesa in 2.2.0; in its absence, we have a simpler implementation that only handles Points
   val st_castToGeometry: UserDefinedFunction = udf { pt: Point => pt.asInstanceOf[Geometry] }
+
+  def box2d(geom: Column): Column =
+    when(
+      geom.isNotNull and not(st_isEmpty(geom)),
+      when(st_geometryType(geom) === "Point",
+        struct(st_x(geom) as 'minX,
+          st_y(geom) as 'minY,
+          st_x(geom) as 'maxX,
+          st_y(geom) as 'maxY)).otherwise(struct(
+        st_x(st_pointN(st_exteriorRing(st_envelope(geom)), lit(1))) as 'minX,
+        st_y(st_pointN(st_exteriorRing(st_envelope(geom)), lit(1))) as 'minY,
+        st_x(st_pointN(st_exteriorRing(st_envelope(geom)), lit(3))) as 'maxX,
+        st_y(st_pointN(st_exteriorRing(st_envelope(geom)), lit(3))) as 'maxY
+      ))
+    )
 }
