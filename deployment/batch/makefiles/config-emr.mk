a84758f8d8fd2ca59c2ee21b57d95df285b4204f
jpolchlo <jpolchlopek@azavea.com>
2019-07-09 10:33:31 -0400

    Include additional options during EMR cluster creation

--- deployment/batch/makefiles/config-emr.mk
+++ deployment/batch/makefiles/config-emr.mk
@@ -4,11 +4,15 @@ export WORKER_SECURITY_GROUP := sg-02edec736e909d506
 export SERVICE_ACCESS_SG := sg-055b18600d3fc2c50
 export SANDBOX_SG := sg-6b227c23
 
+# Uncomment/edit the followings line to add extra attributes to the cluster creation
+#export MASTER_EMR_ATTRS :=
+export CORE_EMR_ATTRS := EbsConfiguration={EbsOptimized=true,EbsBlockDeviceConfigs=[{VolumeSpecification={VolumeType=gp2,SizeInGB=64}}]}
+
 export MASTER_INSTANCE := m3.xlarge
 export MASTER_PRICE := 0.10
 export WORKER_INSTANCE := m3.xlarge
 export WORKER_PRICE := 0.07
-export WORKER_COUNT := 32
+export WORKER_COUNT := 1
 export USE_SPOT := true
 
 export DRIVER_MEMORY := 10000M
