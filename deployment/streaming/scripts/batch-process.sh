5a551b9268c494c6622ed873c0b2a5ab167a2743
CloudNiner <andy@finkmiller.com>
2020-01-14 17:04:11 -0500

    Refactor batch-process.sh to use json configuration
    This should make it easier to tweak settings and see
    what they are.
    
    Also:
    - Use larger ebs volume for job
    - Make instance types static due to high memory requirements

--- deployment/streaming/scripts/batch-process.sh
+++ deployment/streaming/scripts/batch-process.sh
@@ -14,6 +14,7 @@ set -x
 aws emr create-cluster \
     --applications Name=Ganglia Name=Spark \
     --log-uri ${S3_LOG_URI} \
+		--configurations "file://scripts/emr-configurations/batch-process.json" \
     --ebs-root-volume-size 10 \
     --ec2-attributes "{
 	      \"KeyName\": \"${KEYPAIR}\",
@@ -22,8 +23,8 @@ aws emr create-cluster \
 	      \"EmrManagedMasterSecurityGroup\": \"${MASTER_SECURITY_GROUP}\",
 	      \"EmrManagedSlaveSecurityGroup\": \"${WORKER_SECURITY_GROUP}\",
 	      \"ServiceAccessSecurityGroup\": \"${SERVICE_ACCESS_SG}\",
-              \"AdditionalMasterSecurityGroups\": [\"${SANDBOX_SG}\"],
-              \"AdditionalSlaveSecurityGroups\": [\"${SANDBOX_SG}\"]
+        \"AdditionalMasterSecurityGroups\": [\"${SANDBOX_SG}\"],
+        \"AdditionalSlaveSecurityGroups\": [\"${SANDBOX_SG}\"]
 	    }" \
     --service-role EMR_DefaultRole \
     --release-label emr-5.19.0 \
@@ -33,18 +34,36 @@ aws emr create-cluster \
 	        \"InstanceCount\": 1,
 	        \"BidPrice\": \"OnDemandPrice\",
 	        \"InstanceGroupType\": \"MASTER\",
-	        \"InstanceType\": \"${BATCH_INSTANCE_TYPE}\",
-	        \"Name\":\"Master\"
+	        \"InstanceType\": \"m4.xlarge\",
+	        \"Name\":\"Master\",
+	        \"EbsConfiguration\": {
+						\"EbsOptimized\": true,
+						\"EbsBlockDeviceConfigs\": [{
+							\"VolumeSpecification\": {
+								\"VolumeType\": \"gp2\",
+								\"SizeInGB\": 1024
+							}
+						}]
+					}
 	      }, {
 	        \"InstanceCount\": ${NUM_EXECUTORS},
 	        \"BidPrice\": \"OnDemandPrice\",
 	        \"InstanceGroupType\": \"CORE\",
-	        \"InstanceType\": \"${BATCH_INSTANCE_TYPE}\",
-	        \"Name\":\"Workers\"
+	        \"InstanceType\": \"r5.xlarge\",
+	        \"Name\":\"Workers\",
+	        \"EbsConfiguration\": {
+						\"EbsOptimized\": true,
+						\"EbsBlockDeviceConfigs\": [{
+							\"VolumeSpecification\": {
+								\"VolumeType\": \"gp2\",
+								\"SizeInGB\": 1024
+							}
+						}]
+					}
 	      }
 	    ]" \
     --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
-    --auto-terminate \
+		--auto-terminate \
     --region us-east-1 \
     --steps "[
 	      {
