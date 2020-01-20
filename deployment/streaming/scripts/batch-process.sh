f377838145505428d658e42d41abcb45eac01028
CloudNiner <andy@finkmiller.com>
2020-01-20 09:49:31 -0500

    Fixup whitespace
    batch-process was a very weird, inconsistent
    mix of tab and space whitespace. Just rewrote
    it entirely as two space indented spaces which
    seems to be the most consistent with other
    files.

--- deployment/streaming/scripts/batch-process.sh
+++ deployment/streaming/scripts/batch-process.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ -z ${VERSION_TAG+x} ]; then
-    echo "Do not run this script directly.  Use the Makefile in the parent directory."
-    exit 1
+  echo "Do not run this script directly.  Use the Makefile in the parent directory."
+  exit 1
 fi
 
 CLUSTER_NAME=$1
@@ -12,66 +12,66 @@ ARGS=$4
 
 set -x
 aws emr create-cluster \
-    --applications Name=Ganglia Name=Spark \
-    --log-uri ${S3_LOG_URI} \
-		--configurations "file://scripts/emr-configurations/batch-process.json" \
-    --ebs-root-volume-size 10 \
-    --ec2-attributes "{
-	      \"KeyName\": \"${KEYPAIR}\",
-	      \"InstanceProfile\":\"EMR_EC2_DefaultRole\",
-	      \"SubnetId\": \"${SUBNET}\",
-	      \"EmrManagedMasterSecurityGroup\": \"${MASTER_SECURITY_GROUP}\",
-	      \"EmrManagedSlaveSecurityGroup\": \"${WORKER_SECURITY_GROUP}\",
-	      \"ServiceAccessSecurityGroup\": \"${SERVICE_ACCESS_SG}\",
-        \"AdditionalMasterSecurityGroups\": [\"${SANDBOX_SG}\"],
-        \"AdditionalSlaveSecurityGroups\": [\"${SANDBOX_SG}\"]
-	    }" \
-    --service-role EMR_DefaultRole \
-    --release-label emr-5.19.0 \
-    --name "$CLUSTER_NAME" \
-    --instance-groups "[
-	      {
-	        \"InstanceCount\": 1,
-	        \"BidPrice\": \"OnDemandPrice\",
-	        \"InstanceGroupType\": \"MASTER\",
-	        \"InstanceType\": \"${BATCH_MASTER_INSTANCE_TYPE}\",
-	        \"Name\":\"Master\",
-	        \"EbsConfiguration\": {
-						\"EbsOptimized\": true,
-						\"EbsBlockDeviceConfigs\": [{
-							\"VolumeSpecification\": {
-								\"VolumeType\": \"gp2\",
-								\"SizeInGB\": 1024
-							}
-						}]
-					}
-	      }, {
-	        \"InstanceCount\": ${NUM_EXECUTORS},
-	        \"BidPrice\": \"OnDemandPrice\",
-	        \"InstanceGroupType\": \"CORE\",
-	        \"InstanceType\": \"${BATCH_CORE_INSTANCE_TYPE}\",
-	        \"Name\":\"Workers\",
-	        \"EbsConfiguration\": {
-						\"EbsOptimized\": true,
-						\"EbsBlockDeviceConfigs\": [{
-							\"VolumeSpecification\": {
-								\"VolumeType\": \"gp2\",
-								\"SizeInGB\": 1024
-							}
-						}]
-					}
-	      }
-	    ]" \
-    --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
-		--auto-terminate \
-    --region us-east-1 \
-    --steps "[
-	      {
-	        \"Args\": $ARGS,
-	        \"Type\": \"CUSTOM_JAR\",
-	        \"ActionOnFailure\": \"TERMINATE_CLUSTER\",
-	        \"Jar\": \"command-runner.jar\",
-	        \"Properties\": \"\",
-	        \"Name\": \"$STEP_NAME\"
-	      }
-	    ]"
+  --applications Name=Ganglia Name=Spark \
+  --log-uri ${S3_LOG_URI} \
+  --configurations "file://scripts/emr-configurations/batch-process.json" \
+  --ebs-root-volume-size 10 \
+  --ec2-attributes "{
+      \"KeyName\": \"${KEYPAIR}\",
+      \"InstanceProfile\":\"EMR_EC2_DefaultRole\",
+      \"SubnetId\": \"${SUBNET}\",
+      \"EmrManagedMasterSecurityGroup\": \"${MASTER_SECURITY_GROUP}\",
+      \"EmrManagedSlaveSecurityGroup\": \"${WORKER_SECURITY_GROUP}\",
+      \"ServiceAccessSecurityGroup\": \"${SERVICE_ACCESS_SG}\",
+      \"AdditionalMasterSecurityGroups\": [\"${SANDBOX_SG}\"],
+      \"AdditionalSlaveSecurityGroups\": [\"${SANDBOX_SG}\"]
+    }" \
+  --service-role EMR_DefaultRole \
+  --release-label emr-5.19.0 \
+  --name "$CLUSTER_NAME" \
+  --instance-groups "[
+      {
+        \"InstanceCount\": 1,
+        \"BidPrice\": \"OnDemandPrice\",
+        \"InstanceGroupType\": \"MASTER\",
+        \"InstanceType\": \"${BATCH_MASTER_INSTANCE_TYPE}\",
+        \"Name\":\"Master\",
+        \"EbsConfiguration\": {
+          \"EbsOptimized\": true,
+          \"EbsBlockDeviceConfigs\": [{
+            \"VolumeSpecification\": {
+              \"VolumeType\": \"gp2\",
+              \"SizeInGB\": 1024
+            }
+          }]
+        }
+      }, {
+        \"InstanceCount\": ${NUM_EXECUTORS},
+        \"BidPrice\": \"OnDemandPrice\",
+        \"InstanceGroupType\": \"CORE\",
+        \"InstanceType\": \"${BATCH_CORE_INSTANCE_TYPE}\",
+        \"Name\":\"Workers\",
+        \"EbsConfiguration\": {
+          \"EbsOptimized\": true,
+          \"EbsBlockDeviceConfigs\": [{
+            \"VolumeSpecification\": {
+              \"VolumeType\": \"gp2\",
+              \"SizeInGB\": 1024
+            }
+          }]
+        }
+      }
+    ]" \
+  --scale-down-behavior TERMINATE_AT_TASK_COMPLETION \
+  --auto-terminate \
+  --region us-east-1 \
+  --steps "[
+      {
+        \"Args\": $ARGS,
+        \"Type\": \"CUSTOM_JAR\",
+        \"ActionOnFailure\": \"TERMINATE_CLUSTER\",
+        \"Jar\": \"command-runner.jar\",
+        \"Properties\": \"\",
+        \"Name\": \"$STEP_NAME\"
+      }
+    ]"
